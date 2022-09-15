0cafb832f1edd8c8e29e5cf037779cd9794278ca
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2022-09-15 18:39:37 +0530

    Time: 4 ms (46.47%), Space: 5.9 MB (84.71%) - LeetHub

--- 202-happy-number/202-happy-number.cpp
+++ 202-happy-number/202-happy-number.cpp
@@ -2,15 +2,44 @@ class Solution {
 public:
     bool isHappy(int n) 
     {
-        if(n==1 || n==7) //failed test case for n==7 so added here ckeckeed for other single digit->works
+//         if(n==1 || n==7) //failed test case for n==7 so added here ckeckeed for other single digit->works
+//         {
+//             return true;
+//         }
+//         if(n%10==n)
+//         {
+//             return false;
+//         }
+        
+//         int sum=0;
+//         do
+//         {
+//             sum=sum+pow((n%10),2);
+//             n=n/10;
+//         }
+//         while(n>0);
+        
+//         return isHappy(sum);
+        
+        if(n==1 || n==7)  //interesting two-pointer approach
         {
             return true;
         }
-        if(n%10==n)
+        
+        int slow=square(n);
+        int fast=square(slow);
+        
+        while(slow!=fast)
         {
-            return false;
+            slow=square(slow);
+            fast=square(square(fast));
         }
         
+        return slow==1;
+    }
+    
+    int square(int n)
+    {
         int sum=0;
         do
         {
@@ -19,6 +48,6 @@ public:
         }
         while(n>0);
         
-        return isHappy(sum);
+        return sum;
     }
 };
