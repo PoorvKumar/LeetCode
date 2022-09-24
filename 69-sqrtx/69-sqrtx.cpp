6908455a6761bfd01ea8f46e43854b92ecce7aae
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2022-09-24 08:57:10 +0530

    Time: 3 ms (73.82%), Space: 5.9 MB (73.61%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int mySqrt(int x) 
+    {
+        unsigned long low=1;
+        unsigned long high=x;
+        unsigned long mid=(low+high)/2;
+        unsigned int M=1000000007;
+        
+        while(low<=high)
+        {
+            unsigned long sqr=mid*mid;
+            
+            if(sqr==x)
+            {
+                return mid;
+            }
+            else if(sqr>x)
+            {
+                high=mid-1;
+                mid=(low+high)/2;
+            }
+            else 
+            {
+                low=mid+1;
+                mid=(low+high)/2;
+            }
+        }
+        // cout<<low<<" "<<high<<" "<<mid<<endl;
+        
+        return mid;
+    }
+};
