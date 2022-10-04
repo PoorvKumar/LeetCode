efc2819deb2d136b3338cc5a9001bb21b4c6306b
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2022-10-04 10:27:11 +0530

    Time: 24 ms (30.13%), Space: 21.1 MB (92.08%) - LeetHub

--- 112-path-sum/112-path-sum.cpp
+++ 112-path-sum/112-path-sum.cpp
@@ -11,27 +11,6 @@
  */
 class Solution {
 public:
-//     bool PathSum(TreeNode* root,int targetSum)  //made this to deal with  
-//     {                                           // root==NULL and targetSum==0 testcase
-//         int sum=0;
-        
-//         if(root==NULL)
-//         {
-//             if(targetSum!=0)
-//             {
-//                 return false;
-//             }
-//             else
-//             {
-//                 return true;
-//             }
-//         }
-        
-//         sum=root->val;
-        
-//         return PathSum(root->left,targetSum-sum) || PathSum(root->right,targetSum-sum);
-//     }
-    
     bool hasPathSum(TreeNode* root, int targetSum) 
     {
         if(root==NULL)
@@ -47,16 +26,6 @@ public:
             }
             return false;
         }
-        
-//         if(root->left==NULL) //right skewed
-//         {
-//             return hasPathSum(root->right,targetSum-root->val);
-//         }
-        
-//         if(root->right==NULL) //left skewed
-//         {
-//             return hasPathSum(root->left,targetSum-root->val);
-//         }
         
         return hasPathSum(root->left,targetSum-root->val) || hasPathSum(root->right,targetSum-root->val);
     }
