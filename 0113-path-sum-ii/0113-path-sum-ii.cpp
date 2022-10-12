242c9316c93d9f3a5c11008faf6d35db38cfdaff
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2022-10-12 19:45:03 +0530

    Time: 31 ms (26.24%), Space: 36.5 MB (24.48%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.cpp
+++ 0113-path-sum-ii/0113-path-sum-ii.cpp
@@ -69,4 +69,10 @@ public:
         
         return pathSum;
     }
-};
+};  
+
+//one thing to note is do not overthink and after learning a concept do not stick to it while solving problem
+//be flexible and think like a genius while solving problems 
+
+//here in the begining I learnt DFS recursively using stack in Graph and was thinking how to do it using stack or queue
+//also give enough quality time to a problem and think deeply for several testcases without rushing and only see hint if no idea for where to start
