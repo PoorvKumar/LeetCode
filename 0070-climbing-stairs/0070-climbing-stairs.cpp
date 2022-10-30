a55ed334532dad7881af6ce8e5e87d4246780f80
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2022-10-31 01:44:01 +0530

    Time: 2 ms (43.46%), Space: 6.1 MB (31.03%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -8,19 +8,39 @@ public:
         umap.clear();
     }
     
-    int climbStairs(int n) 
-    {        
+//     int climbStairs(int n) // Top-Down DP -> Recursion + Memoization
+//     {                      // could be done with better space complexity
+//         if(n<3)
+//         {
+//             return n; 
+//         }
+//                                 //Time Complexity -> O(n)        
+//         if(umap.count(n)==1)    // Space Complexity -> O(n) + O(n)
+//         {
+//             return umap[n];
+//         }
+        
+//         umap[n]=climbStairs(n-1)+climbStairs(n-2);    
+//         return umap[n];
+//     }
+    
+    int climbStairs(int n) //Bottom-Up DP -> Tabulation
+    {
         if(n<3)
         {
             return n;
         }
         
-        if(umap.count(n)==1)
+        vector<int> dp(n+1);
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=2;                   // Time Complexity -> O(n)
+                                   // Space Complexity -> O(n)        
+        for(int i=3; i<=n; i++)
         {
-            return umap[n];
+            dp[i]=dp[i-1]+dp[i-2];
         }
         
-        umap[n]=climbStairs(n-1)+climbStairs(n-2);    
-        return umap[n];
+        return dp[n];
     }
 };
