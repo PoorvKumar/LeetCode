b5dda216d992bb4189b418f1fe56c5d1102fc8fb
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2022-10-31 01:33:18 +0530

    Time: 7 ms (5.96%), Space: 6.5 MB (15.67%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,25 @@
+class Solution 
+{
+private:
+    unordered_map<int,int> umap;
+public:
+    Solution()
+    {
+        umap.clear();
+    }
+    int climbStairs(int n) 
+    {        
+        if(n<3)
+        {
+            return n;
+        }
+        
+        if(umap.count(n)==1)
+        {
+            return umap[n];
+        }
+        
+        umap[n]=climbStairs(n-1)+climbStairs(n-2);    
+        return umap[n];
+    }
+};
