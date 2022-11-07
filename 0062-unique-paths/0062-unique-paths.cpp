74a582b83e31bd8778c57516b72c3795c42a4d19
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2022-11-07 14:20:30 +0530

    Time: 7 ms (10.11%), Space: 10.5 MB (29.77%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -4,13 +4,7 @@ private:
     unordered_map<int,unordered_map<int,int>> umap;
 public:
     int uniquePaths(int m, int n) 
-    {
-        // if(m==1 && n==1)
-        // {
-        //     umap[m][n]=1;
-        //     return umap[m][n];
-        // }
-        
+    {        
         if(m==1 || n==1)
         {
             umap[m][n]=1;
@@ -24,5 +18,7 @@ public:
         
         umap[m][n]=uniquePaths(m-1,n)+uniquePaths(m,n-1);
         return umap[m][n];
+        
+        
     }
 };
