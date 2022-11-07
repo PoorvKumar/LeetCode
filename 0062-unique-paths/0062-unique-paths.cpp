3942dab5afdb2e043c7d55905fedd696d67ca0b3
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2022-11-07 14:49:39 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (29.77%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -5,20 +5,48 @@ private:
 public:
     int uniquePaths(int m, int n) 
     {        
+//         //top-down DP approach-> Recursion + Memomization
+//         if(m==1 || n==1)  
+//         {
+//             umap[m][n]=1;
+//             return umap[m][n];
+//         }
+        
+//         if(umap.count(m)==1 && umap[m].count(n)==1)
+//         {
+//             return umap[m][n];
+//         }
+        
+//         umap[m][n]=uniquePaths(m-1,n)+uniquePaths(m,n-1);
+//         return umap[m][n];
+        
+        // bottom-up DP approach-> Tabulation
         if(m==1 || n==1)
         {
-            umap[m][n]=1;
-            return umap[m][n];
+            return 1;
         }
         
-        if(umap.count(m)==1 && umap[m].count(n)==1)
+        vector<vector<int>> tab(m+1,vector<int>(n+1));
+        
+        for(int i=1; i<m; i++)
         {
-            return umap[m][n];
+            for(int j=1; j<n; j++)
+            {
+                if(i==1 || j==1)
+                {
+                    tab[i][j]=1;
+                }
+            }
         }
         
-        umap[m][n]=uniquePaths(m-1,n)+uniquePaths(m,n-1);
-        return umap[m][n];
+        for(int i=2; i<=m; i++)
+        {
+            for(int j=2; j<=n; j++)
+            {
+                tab[i][j]=tab[i-1][j]+tab[i][j-1];
+            }
+        }
         
-        
+        return tab[m][n]+2;
     }
 };
