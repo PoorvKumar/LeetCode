298a48f06a8c6cbc7889f8dcd4ff9bcad6bd7b70
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2022-11-19 13:29:08 +0530

    Time: 508 ms (59.72%), Space: 174.1 MB (36.34%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,65 +1,123 @@
 class LRUCache 
 {
 private:
-    unordered_map<int ,pair<int,int>> lru;
-    set<pair<int,int>> ageBit;
+    // unordered_map<int ,pair<int,int>> lru;
+    // set<pair<int,int>> ageBit;
     int capacity;
-    int age=0;
+    // int age=0;
+    
+    unordered_map<int,pair<int,list<int>::iterator>> cache;
+    list<int> rank;
     
 public:
     LRUCache(int capacity) 
     {
         this->capacity=capacity;
-        age=0;
+        // age=0;
     }
     
-    int get(int key) 
+    int get(int key)
     {
-        int x=-1;
-        if(!lru.count(key))
+        list<int>::iterator it;
+        
+        if(!cache.count(key))
         {
             return -1;
         }
-        int agebit=lru[key].first;
-        x=lru[key].second;
-        lru[key].first=++age;
         
-        ageBit.erase(ageBit.find({agebit,key}));
-        ageBit.insert(pair<int,int>(age,key));
+        it=cache[key].second;
+        rank.erase(it);
         
-        return x;
+        rank.push_front(key);
+        
+        cache[key].second=rank.begin();
+        
+        return cache[key].first;
     }
     
-    void put(int key, int value) 
+    void put(int key,int value)
     {
-        if(lru.count(key))
+        list<int>::iterator it;
+        
+        if(cache.count(key))
         {
-            int x=lru[key].first;
-            lru[key].first=++age;
-            lru[key].second=value;
+            it=cache[key].second;
             
-            ageBit.erase(ageBit.find({x,key}));
-            ageBit.insert({age,key});
+            rank.erase(it);
+            rank.push_front(key);//updating rank to highest
+            
+            it=rank.begin();
+            
+            cache[key]={value,rank.begin()}; //value, rank updated
+            
             return;
         }
         
-        if(lru.size()==capacity)
+        if(cache.size()==capacity)
         {
-            int xkey=(*ageBit.begin()).second;
-            ageBit.erase(ageBit.begin());
-            lru.erase(xkey);
+            cache.erase(rank.back());    //removed lru element
+            rank.pop_back();
             
-            lru[key].first=++age;
-            lru[key].second=value;
-            ageBit.insert({age,key});
+            rank.push_front(key);
+            
+            cache[key]={value,rank.begin()};  //assigned highest rank
+            
             return;
         }
         
-        lru[key].first=++age;
-        lru[key].second=value;
-        ageBit.insert({age,key});
+        rank.push_front(key);
+        cache[key]={value,rank.begin()};
+        
         return;
     }
+    
+//     int get(int key) 
+//     {
+//         int x=-1;
+//         if(!lru.count(key))
+//         {
+//             return -1;
+//         }
+//         int agebit=lru[key].first;
+//         x=lru[key].second;
+//         lru[key].first=++age;
+        
+//         ageBit.erase(ageBit.find({agebit,key}));
+//         ageBit.insert(pair<int,int>(age,key));
+        
+//         return x;
+//     }
+    
+//     void put(int key, int value) 
+//     {
+//         if(lru.count(key))
+//         {
+//             int x=lru[key].first;
+//             lru[key].first=++age;
+//             lru[key].second=value;
+            
+//             ageBit.erase(ageBit.find({x,key}));
+//             ageBit.insert({age,key});
+//             return;
+//         }
+        
+//         if(lru.size()==capacity)
+//         {
+//             int xkey=(*ageBit.begin()).second;
+//             ageBit.erase(ageBit.begin());
+//             lru.erase(xkey);
+            
+//             lru[key].first=++age;
+//             lru[key].second=value;
+//             ageBit.insert({age,key});
+//             return;
+//         }
+        
+//         lru[key].first=++age;
+//         lru[key].second=value;
+//         ageBit.insert({age,key});
+//         return;
+//     }
 };
 
 /**
