a3f44b2e9d18be0c0528511fefcce3f99fdc2a1c
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2022-11-21 01:12:28 +0530

    Time: 291 ms (18.17%), Space: 14.4 MB (78.66%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -43,7 +43,24 @@ private:
 public:
     int coinChange(vector<int>& coins, int amount) 
     {
-        int res=coinChangeUtil(coins,amount); //Top-Down DP approach -> Recursion + Memoization
-        return res!=INT_MAX?res:-1;
+        // int res=coinChangeUtil(coins,amount); //Top-Down DP approach -> Recursion + Memoization
+        // return res!=INT_MAX?res:-1;
+        
+        //Bottom-Up DP approach-> Tabulation
+        vector<int> dp(amount+1,INT_MAX);
+        dp[0]=0;
+        
+        for(int i=1; i<=amount; i++)
+        {
+            for(int j=0; j<coins.size(); j++)
+            {
+                if(i-coins[j]>=0 && dp[i-coins[j]]<dp[i])
+                {
+                    dp[i]=1+dp[i-coins[j]];
+                }
+            }
+        }
+        
+        return dp[amount]!=INT_MAX?dp[amount]:-1;
     }
 };
