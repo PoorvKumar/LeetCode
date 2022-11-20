dea11d0f818907b01a499e7706ddb23416bca0b5
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2022-11-21 00:34:47 +0530

    Time: 17 ms (27.70%), Space: 9 MB (32.36%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -3,19 +3,41 @@ class Solution
 private:
     unordered_map<int,unordered_map<int,int>> umap;
 public:
-    int minimumTotal(vector<vector<int>>& triangle,int i=0,int j=0) 
+//     int minimumTotal(vector<vector<int>>& triangle,int i=0,int j=0) 
+//     {
+//         if(i>=triangle.size())  //Top-Down DP approach-> Recursion + Memoizatioon
+//         {
+//             return 0;    
+//         }
+        
+//         if(umap.count(i)==1 && umap[i].count(j))
+//         {
+//             return umap[i][j];
+//         }
+        
+//         umap[i][j]=triangle[i][j]+min(minimumTotal(triangle,i+1,j),minimumTotal(triangle,i+1,j+1));
+//         return umap[i][j];
+//     }
+    
+    int minimumTotal(vector<vector<int>> &triangle)
     {
-        if(i>=triangle.size())  //Top-Down DP approach-> Recursion + Memoizatioon
+        vector<vector<int>> dp(triangle.size(),vector<int>(triangle.size()));
+        
+        int n=triangle.size();
+        
+        for(int i=0; i<triangle[n-1].size(); i++)
         {
-            return 0;    
+            dp[n-1][i]=triangle[n-1][i];
         }
         
-        if(umap.count(i)==1 && umap[i].count(j))
+        for(int i=n-2; i>=0; i--)
         {
-            return umap[i][j];
+            for(int j=0; j<triangle[i].size(); j++)
+            {
+                dp[i][j]=min(dp[i+1][j],dp[i+1][j+1])+triangle[i][j];
+            }
         }
         
-        umap[i][j]=triangle[i][j]+min(minimumTotal(triangle,i+1,j),minimumTotal(triangle,i+1,j+1));
-        return umap[i][j];
+        return dp[0][0];
     }
 };
