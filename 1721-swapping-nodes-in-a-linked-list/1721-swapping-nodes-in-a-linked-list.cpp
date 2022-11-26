d421d286dce2ec19893701d29aa5fdfb92fe9a91
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2022-11-26 10:42:44 +0530

    Time: 701 ms (95.99%), Space: 180.2 MB (15.38%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
@@ -13,41 +13,70 @@ class Solution
 public:
     ListNode* swapNodes(ListNode* head, int k) 
     {
-        ListNode* p=head;
-        ListNode* q=p;
+        //Method 2-> O(n) faster approach saw in discuss section
+        // ListNode* dummyHead=new ListNode(0); //dummyHead not required
+        // dummyHead->next=head;
         
-        int n=0;
-        while(p!=NULL)
+        ListNode* p; //kth from start
+        ListNode* q=head; //kth from end
+        
+        int cnt=k;
+        ListNode* curr=head;
+        
+        while(cnt--)
         {
-            p=p->next;
-            n++;
+            p=curr;  
+            curr=curr->next;
         }
         
-        p=head;
-        
-        //k-1 times to reach kth node from start
-        //n-k from start and n-k-(k-1)==n-2k+1 times from current node for kth node from back
-        
-        int x=k;
-        x--;
-        
-        while(x--)
-        {
-            p=p->next;
-        }
-        
-        q=head;
-        x=n-k;
-        
-        while(x--)
+        while(curr!=NULL)
         {
             q=q->next;
+            curr=curr->next;
         }
         
-        x=q->val;
-        q->val=p->val;
-        p->val=x;
+        int tmp=p->val;
+        p->val=q->val;
+        q->val=tmp;
         
         return head;
+        
+//         //Method 1 -> O(n) first approach
+//         ListNode* p=head;
+//         ListNode* q=p;
+        
+//         int n=0;
+//         while(p!=NULL)
+//         {
+//             p=p->next;
+//             n++;
+//         }
+        
+//         p=head;
+        
+//         //k-1 times to reach kth node from start
+//         //n-k from start and n-k-(k-1)==n-2k+1 times from current node for kth node from back
+        
+//         int x=k;
+//         x--;
+        
+//         while(x--)
+//         {
+//             p=p->next;
+//         }
+        
+//         q=head;
+//         x=n-k;
+        
+//         while(x--)
+//         {
+//             q=q->next;
+//         }
+        
+//         x=q->val;
+//         q->val=p->val;
+//         p->val=x;
+        
+//         return head;
     }
 };
