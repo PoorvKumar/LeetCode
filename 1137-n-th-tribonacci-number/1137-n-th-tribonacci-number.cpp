676f81bbde77c80c0fd82568b6777f57dbfe5303
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2022-12-12 22:30:33 +0530

    Time: 5 ms (13.71%), Space: 6 MB (33.10%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -21,21 +21,42 @@ public:
 //         return umap[n]=tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3); //Top-Down DP approach -> Recursion + Memoization
         
         //Bottom-Up DP approach -> Tabulation
+//         if(n<3)
+//         {
+//             return n==0?0:1;
+//         }
+        
+//         vector<int> dp(n+1);
+//         dp[0]=0;
+//         dp[1]=1;
+//         dp[2]=1;
+        
+//         for(int i=3; i<=n; i++)
+//         {
+//             dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
+//         }
+        
+//         return dp[n];
+        
+        //Space Optimization
         if(n<3)
         {
             return n==0?0:1;
         }
         
-        vector<int> dp(n+1);
-        dp[0]=0;
-        dp[1]=1;
-        dp[2]=1;
+        int x=0;
+        int y=1;
+        int z=1;
+        int res;
         
         for(int i=3; i<=n; i++)
         {
-            dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
+            res=z+y+x;
+            x=y;
+            y=z;
+            z=res;
         }
         
-        return dp[n];
+        return res;
     }
 };
