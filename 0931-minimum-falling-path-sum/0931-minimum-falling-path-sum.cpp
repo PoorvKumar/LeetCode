e88596e820f69e20e784e6a17b1c26f39494a0c6
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2022-12-13 18:52:13 +0530

    Time: 13 ms (95.69%), Space: 9.9 MB (84.29%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,93 +1,99 @@
 class Solution 
 {
-private:
-    unordered_map<int,unordered_map<int,int>> umap;
-    int n;
-    
-    int helper(vector<vector<int>>& matrix,int i,int j)
-    {
-        if(i>=n)
-        {
-            return 0;
-        }
-        
-        if(j<0 || j>=n)
-        {
-            return INT_MAX;
-        }
-        
-        if(umap.count(i) && umap[i].count(j))
-        {
-            return umap[i][j];
-        }
-        
-        for(int k=1; k<n; k++) //going from Bottom-Up 
-        {
-            int minim=min(helper(matrix,i+1,k-1),helper(matrix,i+1,k));
-            minim=min(minim,helper(matrix,i+1,k+1));
-            
-            //                  fun(i,j)
-            //               /      |     \
-            //              /       |      \
-            //    fun(i+1,j-1) fun(i+1,j) fun(i+1,j+1)
-            
-            umap[i][k-1]=matrix[i][k-1]+minim;
-        }
-        
-        return 0;
-    }
-    
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) 
     {
         int n=matrix.size(); 
         
-//         helper(matrix,0,0);
+//         dpTopDown.assign(n,vector<int>(n,-1));
+        
+//         helper(matrix,n-1,0);
 //         int minim=INT_MAX;
+        
+//         for(auto x:dpTopDown)
+//         {
+//             for(auto y:x)
+//             {
+//                 cout<<y<<" ";
+//             }
+//             cout<<endl;
+//         }
         
 //         for(int i=0; i<n; i++)
 //         {
-//             minim=minim>umap[0][i]?umap[0][i]:minim;
+//             minim=min(minim,dpTopDown[0][i]);
 //         }
         
 //         return minim;
         
-        vector<vector<int>> dp(n+1,vector<int>(n+2,0)); //Bottom-Up DP approach -> Tabulation
-        for(int i=0; i<n+1; i++)
-        {
-            dp[i][0]=INT_MAX;
-            dp[i][n+1]=INT_MAX;
-        }
+//         vector<vector<int>> dp(n+1,vector<int>(n+2,0)); //Bottom-Up DP approach -> Tabulation
+//         for(int i=0; i<n+1; i++)
+//         {
+//             dp[i][0]=INT_MAX;
+//             dp[i][n+1]=INT_MAX;
+//         }
         
-        for(int i=0; i<n+2; i++)
-        {
-            dp[n][i]=0;
-        }
+//         for(int i=0; i<n+2; i++)
+//         {
+//             dp[n][i]=0;
+//         }
         
-        // for(auto x:dp)
-        // {
-        //     for(auto y:x)
-        //     {
-        //         cout<<y<<" "; 
-        //     }
-        //     cout<<endl;
-        // }
+//         // for(auto x:dp)
+//         // {
+//         //     for(auto y:x)
+//         //     {
+//         //         cout<<y<<" "; 
+//         //     }
+//         //     cout<<endl;
+//         // }
         
-        for(int i=n-1; i>=0; i--)
-        {
-            for(int j=1; j<=n; j++)
-            {
-                int minim=min(dp[i+1][j-1],dp[i+1][j]);
-                minim=min(minim,dp[i+1][j+1]); 
+//         for(int i=n-1; i>=0; i--)
+//         {
+//             for(int j=1; j<=n; j++)
+//             {
+//                 int minim=min(dp[i+1][j-1],dp[i+1][j]);
+//                 minim=min(minim,dp[i+1][j+1]); 
                 
-                dp[i][j]=matrix[i][j-1]+minim;
+//                 dp[i][j]=matrix[i][j-1]+minim;
+//             }
+//         }
+        
+//         int res=INT_MAX;
+//         for(int j=1; j<=n; j++)
+//         {
+//             res=min(res,dp[0][j]);
+//         }
+        
+//         return res;
+        
+        for(int i=n-2; i>=0; i--)
+        {
+            for(int j=0; j<n; j++)
+            {
+                if(j==0 || j==n-1)
+                {
+                    if(j==0)
+                    {
+                        matrix[i][j]=matrix[i][j]+min(matrix[i+1][j],matrix[i+1][j+1]);
+                    }
+                    else
+                    {
+                        matrix[i][j]=matrix[i][j]+min(matrix[i+1][j-1],matrix[i+1][j]);
+                    }
+                }
+                else
+                {
+                    int minim=min(matrix[i+1][j-1],matrix[i+1][j]);
+                    minim=min(minim,matrix[i+1][j+1]);
+                    matrix[i][j]+=minim;
+                }
             }
         }
         
-        int res=INT_MAX;
-        for(int j=1; j<=n; j++)
+        int res=matrix[0][0];
+        for(int i=0; i<n; i++)
         {
-            res=min(res,dp[0][j]);
+            res=min(res,matrix[0][i]);
         }
         
         return res;
