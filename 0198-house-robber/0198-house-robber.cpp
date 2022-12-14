8b2f046eae53476549137ce5d314f62c0faac4c9
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2022-12-14 12:19:55 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (6.39%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -3,32 +3,32 @@ class Solution
 private:
     unordered_map<int,int> umap;
 public:
-//     int rob(vector<int>& nums,int i=0) 
-//     {
-//         if(i>=nums.size())  
-//         {
-//             return 0;
-//         }
+    int rob(vector<int>& nums,int i=0) 
+    {
+        if(i>=nums.size())  
+        {
+            return 0;
+        }
         
-//         if(umap.count(i))  //Top-Down DP approach -> Recursion + Memoization
-//         {
-//             return umap[i];
-//         }
+        if(umap.count(i))  //Top-Down DP approach -> Recursion + Memoization
+        {
+            return umap[i];
+        }
         
-//         umap[i+2]=rob(nums,i+2);
-//         umap[i+1]=rob(nums,i+1);
+        umap[i+2]=rob(nums,i+2);
+        umap[i+1]=rob(nums,i+1);
         
-//         umap[i]=max(nums[i]+umap[i+2],umap[i+1]);
+        umap[i]=max(nums[i]+umap[i+2],umap[i+1]);
         
-//         return umap[i];
+        return umap[i];
         
-//         // return max(nums[i]+rob(nums,i+2),0+rob(nums,i+1));
+        // return max(nums[i]+rob(nums,i+2),0+rob(nums,i+1));
         
-//         //Recursive Solution //Time Complexity -> O(2^n) where n is size of array
-//     }
+        //Recursive Solution //Time Complexity -> O(2^n) where n is size of array
+    }
     
     
-    int rob(vector<int>& nums)
+    int robBottomUp(vector<int>& nums)
     {
         if(nums.size()==1)
         {
