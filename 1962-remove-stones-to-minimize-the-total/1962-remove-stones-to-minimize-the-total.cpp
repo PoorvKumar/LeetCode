2b82871c98bc438b1073b6f1ce353234dc8e6977
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2022-12-28 12:27:11 +0530

    Time: 546 ms (92.41%), Space: 98.8 MB (84.14%) - LeetHub

--- 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.cpp
+++ 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.cpp
@@ -5,18 +5,25 @@ public:
     {
         int sum=accumulate(piles.begin(),piles.end(),0); //[beginAddress,endAddress),initialSum
         
-        multiset<int> s_piles(piles.begin(),piles.end());
+        // multiset<int> s_piles(piles.begin(),piles.end());
         //we can also use object of type class priority_queue for max heap data structure
+        priority_queue<int> s_piles(piles.begin(),piles.end());
         
         while(k--)
         {
-            int x=*s_piles.rbegin();
+            // int x=*s_piles.rbegin();
+            int x=s_piles.top();
+            
             // cout<<x<<endl;
+            
             sum=sum-x;
-            s_piles.erase(s_piles.find(x));
+            // s_piles.erase(s_piles.find(x));
+            s_piles.pop();
+            
             x=x-x/2;
             sum=sum+x;
-            s_piles.insert(x);
+            // s_piles.insert(x);
+            s_piles.push(x);
         }
         
         return sum;
