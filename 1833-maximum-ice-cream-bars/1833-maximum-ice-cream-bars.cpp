6738c4b63e30bebc2adb5d392640f9a94f7ea569
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-01-06 17:36:27 +0530

    Time: 484 ms (13.47%), Space: 76.5 MB (76.68%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -1,39 +1,39 @@
 class Solution 
 {
-private:
-    unordered_map<int,unordered_map<int,int>> umap;
-public:
-    int maxIceCreamUtil(vector<int>& costs, int coins,int index=0)
-    {
-        if(coins<0)
-        {
-            return INT_MIN;
-        }
+// private:
+//     unordered_map<int,unordered_map<int,int>> umap;
+    
+//     int maxIceCreamUtil(vector<int>& costs, int coins,int index=0)
+//     {
+//         if(coins<0)
+//         {
+//             return INT_MIN;
+//         }
         
-        if(coins==0 || index>costs.size()-1)
-        {
-            return 0;
-        }
+//         if(coins==0 || index>costs.size()-1)
+//         {
+//             return 0;
+//         }
         
-        if(!umap.count(coins-costs[index]) && !umap[coins-costs[index]].count(index+1))
-        {
-            umap[coins-costs[index]][index+1]=maxIceCreamUtil(costs,coins-costs[index],index+1);
-        }
+//         if(!umap.count(coins-costs[index]) && !umap[coins-costs[index]].count(index+1))
+//         {
+//             umap[coins-costs[index]][index+1]=maxIceCreamUtil(costs,coins-costs[index],index+1);
+//         }
         
-        if(!umap.count(coins) && !umap[coins].count(index+1))
-        {
-            umap[coins][index+1]=maxIceCreamUtil(costs,coins,index+1);
-        }
+//         if(!umap.count(coins) && !umap[coins].count(index+1))
+//         {
+//             umap[coins][index+1]=maxIceCreamUtil(costs,coins,index+1);
+//         }
         
-        // return max(1+maxIceCream(costs,coins-costs[index],index+1),maxIceCream(costs,coins,index+1)); //Recursive Relation
+//         // return max(1+maxIceCream(costs,coins-costs[index],index+1),maxIceCream(costs,coins,index+1)); //Recursive Relation
         
-        return umap[coins][index]=max(1+umap[coins-costs[index]][index+1],umap[coins][index+1]);
-    }
+//         return umap[coins][index]=max(1+umap[coins-costs[index]][index+1],umap[coins][index+1]);
+//     }
     
 public:
     int maxIceCream(vector<int>& costs,int coins)
     {
-        sort(costs.begin(),costs.end());
+        sort(costs.begin(),costs.end()); //O(nlogn)
         // if(costs[0]>coins)
         // {
         //     return 0;
