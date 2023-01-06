615d327af0d4d7558e9334ec8dd6998b5ebfc222
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-01-06 18:12:26 +0530

    Time: 2 ms (69.47%), Space: 6.2 MB (9.04%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -12,15 +12,27 @@ public:
         
         // return fib(n-1)+fib(n-2);
         
-        if(!umap.count(n-1))
+//         if(!umap.count(n-1))
+//         {
+//             umap[n-1]=fib(n-1);
+//         }
+//         if(!umap.count(n-2))
+//         {
+//             umap[n-2]=fib(n-2);
+//         }
+        
+//         return umap[n]=umap[n-1]+umap[n-2];
+        
+        vector<int> dp(n+1);
+        
+        dp[0]=0;
+        dp[1]=1;
+        
+        for(int i=2; i<=n; i++)
         {
-            umap[n-1]=fib(n-1);
-        }
-        if(!umap.count(n-2))
-        {
-            umap[n-2]=fib(n-2);
+            dp[i]=dp[i-1]+dp[i-2];
         }
         
-        return umap[n]=umap[n-1]+umap[n-2];
+        return dp[n];
     }
 };
