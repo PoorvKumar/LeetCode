82983b416f9eaa0cffcd7cde4f8f2a02a8c0c480
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-01-07 23:35:10 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (56.76%) - LeetHub

--- 2525-categorize-box-according-to-criteria/2525-categorize-box-according-to-criteria.cpp
+++ 2525-categorize-box-according-to-criteria/2525-categorize-box-according-to-criteria.cpp
@@ -4,8 +4,8 @@ public:
     string categorizeBox(int length, int width, int height, int mass) 
     {
         unsigned long long int x=(long long)length*(long long)width*(long long)height;
-        bool bulky=x>=1000000000 || length>=10000 || width>=10000 || height>=10000;
-        // bool bulky=x>=1e9 || length>=1e4 || width>=1e4 || height>=1e4;
+        // bool bulky=x>=1000000000 || length>=10000 || width>=10000 || height>=10000;
+        bool bulky=x>=1e9 || length>=1e4 || width>=1e4 || height>=1e4;
         bool heavy=mass>=100;
         
         if(bulky && heavy)
