522f33d68c36cb57b84b584a4b0bc91199b84702
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-01-09 12:03:47 +0530

    Time: 7 ms (81.69%), Space: 13.6 MB (82.58%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -48,14 +48,15 @@ public:
         n=cost.size(); //Top-Down DP approach-> Recursion+Memoization
         
         // return min(minCostClimbingUtil(cost,0),minCostClimbingUtil(cost,1));
-        // return minCostClimbingUtil(cost);
+        return minCostClimbingUtil(cost);
         
+        //Bottom-Up DP approach -> Tabulation
         // vector<int> dp(n,0);
-        for(int i=n-3; i>=0; i--)
-        {
-            cost[i]=cost[i]+min(cost[i+1],cost[i+2]);
-        }
+//         for(int i=n-3; i>=0; i--)
+//         {
+//             cost[i]=cost[i]+min(cost[i+1],cost[i+2]);
+//         }
         
-        return min(cost[0],cost[1]);
+//         return min(cost[0],cost[1]);
     }
 };
