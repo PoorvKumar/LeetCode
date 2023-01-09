9718a08a1584482fe091f1b33e50bc2fc954900d
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-01-09 11:48:51 +0530

    Time: 27 ms (5.27%), Space: 15.6 MB (5.44%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -4,6 +4,7 @@ private:
     unordered_map<int,int> umap;
     int n;
     
+    //Top-Down DP approach-> Recursion + Memoization
     int minCostClimbingUtil(vector<int>& cost,int i)
     {
         if(umap.count(i)==1)
@@ -16,14 +17,37 @@ private:
             return 0;
         }
         
-        umap[i]=cost[i]+min(minCostClimbingUtil(cost,i+1),minCostClimbingUtil(cost,i+2));
+        if(umap.count(i+1)==0)
+        {
+            umap[i+1]=minCostClimbingUtil(cost,i+1);
+        }
+        if(!umap.count(i+2))
+        {
+            umap[i+2]=minCostClimbingUtil(cost,i+2);
+        }
+        
+        // umap[i]=cost[i]+min(minCostClimbingUtil(cost,i+1),minCOstClimbingUtil(cost,i+2)); //Recurive Solution
+        umap[i]=cost[i]+min(umap[i+1],umap[i+2]);
         return umap[i];
     }
+    
+//     //Bottom-Up DP approach-> Tabulation
+//     int minCostClimbingUtil(vector<int>& cost,int k)
+//     {
+//         int mincost=0;
+        
+//         for(int i=k; i<n-3; i++)
+//         {
+//             mincost=mincost+cost[i]+min(cost[i+1],cost[i+2]);
+//         } 
+        
+//         return mincost;
+//     }
     
 public:
     int minCostClimbingStairs(vector<int>& cost) 
     {
-        n=cost.size();
+        n=cost.size(); //Top-Down DP approach-> Recursion+Memoization
         
         return min(minCostClimbingUtil(cost,0),minCostClimbingUtil(cost,1));
     }
