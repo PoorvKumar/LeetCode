ea120a439347c3ee72f923211613553444516762
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-01-13 01:51:22 +0530

    Time: 570 ms (89.29%), Space: 167.4 MB (94.29%) - LeetHub

--- 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
+++ 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
@@ -4,7 +4,68 @@ private:
     unordered_map<int,vector<int>> adjList;
     // vector<vector<int>> adjList;
     
-    void subTreeUtil(int src,string& labels,vector<int>& ans,vector<int>& countHash) 
+    //     int countSubTreeUtil(int src,string labels,char x,int parent) //getting TLE
+//     {
+//         if(labels[src]==x)
+//         {
+//             labels[src]='-';
+//             int y=countSubTreeUtil(src,labels,x,parent);
+//             // int y=countSubTreeUtil(src,labels,x,visited);
+//             // labels[src]=x;
+//             return y+1;
+//         }
+        
+//         int countLabel=0;
+        
+//         for(int i=0; i<adjList[src].size(); i++)
+//         {
+//             if(adjList[src][i]!=parent)
+//             {
+//                 int y=countSubTreeUtil(adjList[src][i],labels,x,src);
+//                 countLabel=countLabel+y;
+//             }
+//             // if(!visited[adjList[src][i]])
+//             // {
+//             //     visited[adjList[src][i]]=true;
+//             //     int y=countSubTreeUtil(adjList[src][i],labels,x,visited);
+//             //     countLabel=countLabel+y;
+//             // }
+//         }
+        
+//         return countLabel;
+//     }
+    
+    unordered_map<char,int> helper(int src,string& labels,vector<int>& ans)
+    {
+        // if(adjList[src].size()==1) //-1 added at 0 so that adjacency list of 0 always have atleast 2 elements
+        // {
+        //     // ans[src]=1;
+        //     unordered_map<char,int> umap;
+        //     umap[labels[src]]=1;
+        //     return umap;
+        // }
+        
+        unordered_map<char,int> countHash;
+        countHash[labels[src]]=1;
+        
+        // int i=src==0?0:1;
+        int i=1;
+        
+        for( ; i<adjList[src].size(); i++) //parent at 0 index
+        {
+            unordered_map<char,int> umap=helper(adjList[src][i],labels,ans);
+            for(auto y:umap) //problem here is traversing through object of type class unordered_map everytime
+            {
+                countHash[y.first]=countHash[y.first]+y.second;
+            }
+        }
+        
+        ans[src]=1+countHash[labels[src]];
+        countHash[labels[src]]=ans[src];
+        return countHash;
+    }
+    
+    void subTreeUtil(int src,string& labels,vector<int>& ans,vector<int>& countHash) //faster
     {
         // vector<int> countHash(26,0); 
         int prevCount=countHash[labels[src]-97];
@@ -36,7 +97,7 @@ private:
         return countHash;
     }
 public:
-    vector<int> countSubTrees(int n, vector<vector<int>>& edges, string labels)
+    vector<int> countSubTrees(int n, vector<vector<int>>& edges, string labels) //very very important to look submission notes
     {
         // adjList.assign(n,vector<int>());
         adjList[0].push_back(-1); //parent at 0 index assigned -1 so i always starts from 1
@@ -59,9 +120,9 @@ public:
         //     cout<<endl;
         // }
         
-        // vector<int> countHash(26,0);
-        // subTreeUtil(0,labels,ans,countHash);
-        countHelper(0,labels,ans);
+        vector<int> countHash(26,0);
+        subTreeUtil(0,labels,ans,countHash);
+        // countHelper(0,labels,ans);
         
         return ans;
         
