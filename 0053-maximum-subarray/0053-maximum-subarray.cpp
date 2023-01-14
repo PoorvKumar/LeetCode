c735a18a2b5c11472f00a6e7d8d0337dfbdb4cd5
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-01-14 13:32:10 +0530

    Time: 111 ms (92.76%), Space: 67.8 MB (13.82%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,15 +1,17 @@
-class Solution {
+class Solution 
+{
 public:
-    int maxSubArray(vector<int>& nums) //kadane's algorithm
+    int maxSubArray(vector<int>& nums) 
     {
+        //kadane's algorithm
         int sum=0;
         int maxim=nums[0];
-        // maxim=INT_MIN;
         
         for(int i=0; i<nums.size(); i++)
         {
             sum=sum+nums[i];
             maxim=max(maxim,sum);
+            
             if(sum<0)
             {
                 sum=0;
