8075dff9c42196d82024da7a196833f64d1ea395
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-01-23 10:28:29 +0530

    Time: 219 ms (59.13%), Space: 65.5 MB (54.29%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -0,0 +1,49 @@
+class Solution 
+{
+public:
+    int findJudge(int n, vector<vector<int>>& trust)
+    {
+        // unordered_map<int,unordered_set<int>> adjList;
+        vector<vector<int>> adjList(n+1,vector<int>());
+        
+        for(int i=0; i<trust.size(); i++)
+        {
+            int x=trust[i][0];
+            int y=trust[i][1];
+            adjList[x].push_back(y);
+        }
+        
+        // for(auto x:adjList)
+        // {
+        //     // cout<<x.first<<" -> ";
+        //     for(auto y:x)
+        //     {
+        //         cout<<y<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        
+        for(int i=1; i<=n; i++)
+        {
+            if(adjList[i].empty())
+            {
+                bool flag=true;
+                for(int j=1; j<=n; j++)
+                {
+                    if(i!=j)
+                    {
+                        if(find(adjList[j].begin(),adjList[j].end(),i)==adjList[j].end())
+                        {
+                            flag=false;
+                            break;
+                        }
+                    }
+                }
+                
+                return flag?i:-1;
+            }
+        }
+        
+        return -1;
+    }
+};
