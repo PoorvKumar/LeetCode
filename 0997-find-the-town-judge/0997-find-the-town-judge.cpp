d32862693412e12e83c573a7eba723bb80058d95
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-01-23 18:51:55 +0530

    Time: 216 ms (61.80%), Space: 65.4 MB (54.44%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -3,85 +3,85 @@ class Solution
 public:
     int findJudge(int n, vector<vector<int>>& trust)
     {
-        if(n==1)
-        {
-            return 1;
-        }
-        
-        unordered_map<int,unordered_set<int>> adjList;
-        
-        for(auto x:trust)
-        {
-            adjList[x[0]].insert(x[1]);
-        }
-        
-        if(adjList.size()==0)
-        {
-            return -1;
-        }
-        
-        for(int i=1; i<=n; i++)
-        {
-            if(adjList.count(i)==0)
-            {
-                bool flag=true;
-                for(int j=1; j<=n; j++)
-                {
-                    if(i!=j)
-                    {
-                        if(adjList[j].count(i)==0)
-                        {
-                            return -1;
-                        }
-                    }
-                }
-                return i;
-            }
-        }
-        
-        return -1;
-//         vector<vector<int>> adjList(n+1,vector<int>());
-        
-//         for(int i=0; i<trust.size(); i++)
+//         if(n==1)
 //         {
-//             int x=trust[i][0];
-//             int y=trust[i][1];
-//             adjList[x].push_back(y);
+//             return 1;
 //         }
         
-//         // for(auto x:adjList)
-//         // {
-//         //     // cout<<x.first<<" -> ";
-//         //     for(auto y:x)
-//         //     {
-//         //         cout<<y<<" ";
-//         //     }
-//         //     cout<<endl;
-//         // }
+//         unordered_map<int,unordered_set<int>> adjList;
+        
+//         for(auto x:trust)
+//         {
+//             adjList[x[0]].insert(x[1]);
+//         }
+        
+//         if(adjList.size()==0)
+//         {
+//             return -1;
+//         }
         
 //         for(int i=1; i<=n; i++)
 //         {
-//             if(adjList[i].empty())
+//             if(adjList.count(i)==0)
 //             {
 //                 bool flag=true;
 //                 for(int j=1; j<=n; j++)
 //                 {
 //                     if(i!=j)
 //                     {
-//                         if(find(adjList[j].begin(),adjList[j].end(),i)==adjList[j].end())
+//                         if(adjList[j].count(i)==0)
 //                         {
-//                             return-1;
-//                             // flag=false;
-//                             // break;
+//                             return -1;
 //                         }
 //                     }
 //                 }
-                
 //                 return i;
-//                 // return flag?i:-1;
 //             }
 //         }
         
 //         return -1;
+        vector<vector<int>> adjList(n+1,vector<int>());
+        
+        for(int i=0; i<trust.size(); i++)
+        {
+            int x=trust[i][0];
+            int y=trust[i][1];
+            adjList[x].push_back(y);
+        }
+        
+        // for(auto x:adjList)
+        // {
+        //     // cout<<x.first<<" -> ";
+        //     for(auto y:x)
+        //     {
+        //         cout<<y<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        
+        for(int i=1; i<=n; i++)
+        {
+            if(adjList[i].empty())
+            {
+                // bool flag=true;
+                for(int j=1; j<=n; j++)
+                {
+                    if(i!=j)
+                    {
+                        if(find(adjList[j].begin(),adjList[j].end(),i)==adjList[j].end())
+                        {
+                            return-1;
+                            // flag=false;
+                            // break;
+                        }
+                    }
+                }
+                
+                return i;
+                // return flag?i:-1;
+            }
+        }
+        
+        return -1;
     }
 };
