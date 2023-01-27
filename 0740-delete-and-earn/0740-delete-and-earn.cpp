fd9e91edc9cfc80884819201c6f477f711f0db85
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-01-27 16:17:35 +0530

    Time: 175 ms (5.03%), Space: 13 MB (61.81%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.cpp
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -1,9 +1,46 @@
 class Solution 
 {
-public:
-    int deleteAndEarn(vector<int>& nums) 
+private:
+    unordered_map<int,int> umap;
+    unordered_map<int,int> ans;
+    
+    int deleteAndEarnUtil(vector<int>& vec,int index,int cost,unordered_set<int>& uset)
     {
-        unordered_map<int,int> umap;
+        if(index>=vec.size())
+        {
+            return cost;
+        }
+        
+        // if(ans.count(index)) //wrionog answer
+        // {
+        //     return ans[index];
+        // }
+        
+        int val=vec[index]*umap[vec[index]]; //Time Complexity : O(2^n) every time 2 calls for deleted notdeleted
+        
+        int deleted=0;
+        if(!uset.count(vec[index]-1))
+        {
+            uset.insert(vec[index]);
+            deleted=deleteAndEarnUtil(vec,index+1,cost+val,uset);
+            uset.erase(vec[index]);
+        }
+        else
+        {
+            deleted=0;
+        }
+        
+        int notdeleted=deleteAndEarnUtil(vec,index+1,cost,uset);
+        
+        return max(deleted,notdeleted); //getting TLE 
+        
+        // ans[index]=max(deleted,notdeleted);
+        // return ans[index];
+    }
+public:
+    int deleteAndEarn(vector<int>& nums)
+    {
+        // unordered_map<int,int> umap;
         for(int i=0; i<nums.size(); i++)
         {
             umap[nums[i]]++;
@@ -28,43 +65,72 @@ public:
         vector<int> dp(n,0);
         dp[0]=vec[0]*umap[vec[0]];
         
-        int i=1;
-        
-        if(vec[1]==vec[0]+1)
-        {
-            dp[1]=vec[1]*umap[vec[1]];
-            i++;
-        }
-        
-        maxim=max(dp[0],dp[1]);
-        
-        for( ; i<n; i++)
+        for(int i=1; i<n; i++)
         {
             int x=vec[i]*umap[vec[i]];
             if(vec[i]!=vec[i-1]+1)
             {
+                // dp[i]=dp[i-1]+x; //max at dp[i-1]
                 int maxLast=0;
-                for(int j=0; j<=i-1; j++) //maximum value till i-1
+                for(int j=0; j<i; j++)
                 {
                     maxLast=max(maxLast,dp[j]);
                 }
                 dp[i]=maxLast+x;
-                // dp[i]=dp[i-1]+x;
             }
             else
             {
                 int maxLast=0;
-                for(int j=0; j<i-1; j++) //maximum value till i-2 
+                for(int j=0; j<i-1; j++)
                 {
                     maxLast=max(maxLast,dp[j]);
                 }
                 dp[i]=maxLast+x;
-                // dp[i]=dp[i-2]+x;
             }
-            maxim=max(maxim,dp[i]);
         }
         
-        return maxim;
+        return max(dp[n-1],dp[n-2]);
+//         int i=1;
+        
+//         if(vec[1]==vec[0]+1)
+//         {
+//             dp[1]=vec[1]*umap[vec[1]];
+//             i++;
+//         }
+        
+//         maxim=max(dp[0],dp[1]);
+        
+//         for( ; i<n; i++) //Time Complexity: O(n^2)
+//         {
+//             int x=vec[i]*umap[vec[i]];
+//             if(vec[i]!=vec[i-1]+1)
+//             {
+//                 int maxLast=0;
+//                 for(int j=0; j<=i-1; j++) //maximum value till i-1
+//                 {
+//                     maxLast=max(maxLast,dp[j]);
+//                 }
+//                 dp[i]=maxLast+x;
+//                 // dp[i]=dp[i-1]+x;
+//             }
+//             else
+//             {
+//                 int maxLast=0;
+//                 for(int j=0; j<i-1; j++) //maximum value till i-2 
+//                 {
+//                     maxLast=max(maxLast,dp[j]);
+//                 }
+//                 dp[i]=maxLast+x;
+//                 // dp[i]=dp[i-2]+x;
+//             }
+//             maxim=max(maxim,dp[i]);
+//         }
+        
+//         return maxim;
         // return max(dp[n-1],dp[n-2]);
+        
+//         unordered_set<int> uset;
+        
+//         return deleteAndEarnUtil(vec,0,0,uset);
     }
 };
