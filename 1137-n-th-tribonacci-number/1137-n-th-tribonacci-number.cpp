fd41d4eb72ba6df26ebb3f5c31e1fd258b93fb00
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-01-30 11:44:52 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (6.99%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -5,58 +5,30 @@ private:
 public:
     int tribonacci(int n) 
     {
-        //Recursion && Top-Down DP approach -> Recursion + Memoization
-//         if(n<3)
-//         {
-//             return n==0?0:1;
-//         }
-        
-//         if(umap.count(n))
-//         {
-//             return umap[n];
-//         }
-        
-//         // return tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3); //Recursive Solution
-        
-//         return umap[n]=tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3); //Top-Down DP approach -> Recursion + Memoization
-        
-        //Bottom-Up DP approach -> Tabulation
-//         if(n<3)
-//         {
-//             return n==0?0:1;
-//         }
-        
-//         vector<int> dp(n+1);
-//         dp[0]=0;
-//         dp[1]=1;
-//         dp[2]=1;
-        
-//         for(int i=3; i<=n; i++)
-//         {
-//             dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
-//         }
-        
-//         return dp[n];
-        
-        //Space Optimization
+        if(n<=0)
+        {
+            return 0;
+        }
         if(n<3)
         {
-            return n==0?0:1;
+            return 1;
         }
         
-        int x=0;
-        int y=1;
-        int z=1;
-        int res;
-        
-        for(int i=3; i<=n; i++)
+        if(!umap.count(n-1))
         {
-            res=z+y+x;
-            x=y;
-            y=z;
-            z=res;
+            umap[n-1]=tribonacci(n-1);
+        }
+        if(!umap.count(n-2))
+        {
+            umap[n-2]=tribonacci(n-2);
+        }
+        if(!umap.count(n-3))
+        {
+            umap[n-3]=tribonacci(n-3);
         }
         
-        return res;
+        // return tribonacci(n-3)+tribonacci(n-2)+tribonacci(n-1);
+        umap[n]=umap[n-3]+umap[n-2]+umap[n-1];
+        return umap[n];
     }
 };
