48f0ae2eac5d3d7afd4c12294dfe7df9a12bbd02
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-01-30 11:49:03 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (6.99%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -14,21 +14,33 @@ public:
             return 1;
         }
         
-        if(!umap.count(n-1))
+//         if(!umap.count(n-1))
+//         {
+//             umap[n-1]=tribonacci(n-1);
+//         }
+//         if(!umap.count(n-2))
+//         {
+//             umap[n-2]=tribonacci(n-2);
+//         }
+//         if(!umap.count(n-3))
+//         {
+//             umap[n-3]=tribonacci(n-3);
+//         }
+        
+//         // return tribonacci(n-3)+tribonacci(n-2)+tribonacci(n-1); //Recursive Solution
+//         umap[n]=umap[n-3]+umap[n-2]+umap[n-1]; //Top-Down DP approach -> Recursion + Memoization
+//         return umap[n];
+        
+        vector<int> dp(n+1);
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=1;
+        
+        for(int i=3; i<=n; i++)
         {
-            umap[n-1]=tribonacci(n-1);
-        }
-        if(!umap.count(n-2))
-        {
-            umap[n-2]=tribonacci(n-2);
-        }
-        if(!umap.count(n-3))
-        {
-            umap[n-3]=tribonacci(n-3);
+            dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
         }
         
-        // return tribonacci(n-3)+tribonacci(n-2)+tribonacci(n-1);
-        umap[n]=umap[n-3]+umap[n-2]+umap[n-1];
-        return umap[n];
+        return dp[n];
     }
 };
