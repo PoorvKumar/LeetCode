18959701f51e19680c93ffbb0fb77d83c9a86f1d
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-01-31 21:52:30 +0530

    Time: 29 ms (81.46%), Space: 13.5 MB (46.29%) - LeetHub

---
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -0,0 +1,65 @@
+class Solution 
+{
+public:
+    int maxPoints(vector<vector<int>>& points) 
+    {
+        if(points.size()==1)
+        {
+            return 1;
+        }
+        
+        int ans=2;
+        // unordered_map<double,int> umap;
+        
+        for(int i=0; i<points.size(); i++)
+        {
+            unordered_map<double,int> umap; //if max points lie on line with curr point 
+            int x=points[i][0];
+            int y=points[i][1];
+            
+            for(int j=i+1; j<points.size(); j++)
+            {
+                int a=points[j][0];
+                int b=points[j][1];
+                // double m=atan2(b-y,a-x);
+                // umap[m]++;
+                
+                double m;
+                if(a!=x)
+                {
+                    m=(double)(b-y)/(double)(a-x);
+                    // m=atan2(b-y,a-x);
+                }
+                else
+                {
+                    m=10001;
+                }
+                umap[m]++;
+            }
+            
+            for(auto p:umap)
+            {
+                ans=max(ans,p.second+1); 
+                // cout<<p.first<<" "<<p.second<<endl;
+            }
+        }
+        
+//         int ans=0;
+        
+//         for(auto x:umap)
+//         {
+//             ans=max(ans,x.second);
+//             // cout<<x.first<<" "<<x.second<<endl;
+//         }
+        
+//         // cout<<ans<<endl;
+        
+//         //can solve using quadratic formula
+//         int x1=(1+pow(1+8*ans,0.5))/2;
+//         // int x2=(1+pow(1-8*ans,0.5))/2;
+        
+//         return x1;
+        
+        return ans;
+    }
+};
