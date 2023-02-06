774b33592a315e802bdc13346b789f28c4378498
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-02-06 23:12:12 +0530

    Time: 3 ms (94.23%), Space: 17 MB (31.28%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,29 +1,9 @@
 class Solution 
 {
 private:
-    bool compareVec(vector<int>& v1,vector<int>& v2)
+    void combinationSumUtil(vector<int>& c,vector<vector<int>>& res,vector<int>& vec,int target,int index)
     {
-        if(v1.size()!=v2.size())
-        {
-            return false;
-        }
-        
-        // sort(v2.begin(),v2.end());
-        
-        for(int i=0; i<v1.size(); i++)
-        {
-            if(v1[i]!=v2[i])
-            {
-                return false;
-            }
-        }
-        
-        return true;
-    }
-    
-    void combinationSumUtil(vector<int>& c,vector<vector<int>>& res,vector<int>& vec,int target)
-    {
-        if(target<0)
+        if(target<0 || index>=c.size())
         {
             return ;
         }
@@ -31,65 +11,27 @@ private:
         if(target==0)
         {
             res.push_back(vec);
-            return;
+            return ;
         }
         
-        for(auto x:c)
-        {
-            if(target>=x)
-            {
-                vec.push_back(x);
-                // cout<<x<<" "<<target<<endl;
-                combinationSumUtil(c,res,vec,target-x);
-                vec.pop_back();
-            }            
-        }
+        //not include
+        combinationSumUtil(c,res,vec,target,index+1);
+        
+        //include
+        vec.push_back(c[index]);
+        combinationSumUtil(c,res,vec,target-c[index],index);
+        vec.pop_back();
         
         return ;
     }
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) 
     {
-        sort(candidates.begin(),candidates.end());
-        
         vector<vector<int>> res;
         vector<int> vec;
         
-        combinationSumUtil(candidates,res,vec,target);
+        combinationSumUtil(candidates,res,vec,target,0);
         
-        if(res.empty())
-        {
-            return res;
-        }
-        
-        for(auto &x:res)
-        {
-            sort(x.begin(),x.end());
-        }
-        
-        sort(res.begin(),res.end());
-        
-        vector<vector<int>> ans;
-        // ans.push_back(res[0]);
-        
-        for(auto x:res)
-        {
-            bool flag=true;
-            for(auto y:ans)
-            {
-                if(compareVec(x,y))
-                {
-                    flag=false;
-                    break;
-                }
-            }
-            if(flag)
-            {
-                ans.push_back(x);
-            }
-        }
-         
-        // return res;
-        return ans;
+        return res;
     }
 };
