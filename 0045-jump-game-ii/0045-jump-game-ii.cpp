62b120c1a5ed3c1af8ba12b7b65cd8976d67ef11
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-02-08 10:29:38 +0530

    Time: 1900 ms (5.01%), Space: 25.4 MB (6.85%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -3,67 +3,45 @@ class Solution
 private:
     unordered_map<int,int> umap;
     
-    int jumpHelper(vector<int>& nums,int index) //Top-Down DP approach -> Recursion + Memoization
+    int jumpHelper(vector<int>& nums,int index)
     {
-        if(index>=nums.size()-1)
+        if(index>=nums.size()-1) //base case
         {
-            umap[nums.size()-1]=0;
             return 0;
+        }
+        
+        if(umap.count(index))
+        {
+            return umap[index];
         }
         
         int minim=INT_MAX;
         
-        for(int i=1; i<=nums[index]; i++)
+        for(int i=1; i<=nums[index]; i++) //Recursive Solution
         {
+            int x;
             if(!umap.count(index+i))
             {
-                umap[index+i]=jumpHelper(nums,index+i);
+                x=jumpHelper(nums,index+i);
             }
-            
-            minim=min(minim,umap[index+i]);
+            else
+            {
+                x=umap[index+i];
+            }
+            if(x<INT_MAX)
+            {
+                x++;
+            }
+            minim=min(minim,x);
         }
         
-        umap[index]=minim<INT_MAX?1+minim:minim;
-        
-        return umap[index];
+        return umap[index]=minim;
     }
 public:
     int jump(vector<int>& nums) 
     {
-        // return jumpHelper(nums,0); //Top-Down DP approach -> Recursion + Memoization
+        int ans=jumpHelper(nums,0);
         
-        //Bottom-Up DP approach -> Tabulation
-        
-        vector<int> dp(nums.size(),INT_MAX);
-        dp[nums.size()-1]=0;
-        
-        for(int i=nums.size()-2; i>=0; i--)
-        {
-            int x=INT_MAX;
-            for(int j=1; j<=nums[i]; j++)
-            {
-                if(i+j>=nums.size()-1)
-                {
-                    dp[i]=1;
-                    break;
-                }
-                else
-                {
-                    x=min(x,dp[i+j]);
-                    // cout<<x<<" "<<i<<endl;
-                    if(x!=INT_MAX)
-                    {
-                        dp[i]=1+x;
-                    }
-                }
-            }
-        }
-        
-        // for(auto x:dp)
-        // {
-        //     cout<<x<<" ";
-        // }
-        
-        return dp[0];
+        return ans!=INT_MAX?ans:-1;
     }
 };
