cbfe7d3752dda8cbd3d7c092b0e5ed04df1708a3
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-02-14 00:17:22 +0530

    Time: 129 ms (44.85%), Space: 6.8 MB (97.48%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -24,14 +24,14 @@ public:
         
         for(int i=0; i<n; i++) //O(n^3) with some optimisation
         {
-            for(int j=n-1; j>i; j--)
+            for(int j=n-1; j>i && j-i+1>str.length(); j--)
             {
                 if(s[i]==s[j] && isPalindrome(s,i,j))
                 {
                     if(j-i+1>str.length())
                     {
                         str=s.substr(i,j-i+1); //s.substr(start_index,numbe_of_characters);
-                        break;
+                        // break;
                     }
                 }
             }
