981a7177d74ff1827b9e2a96ea513e9e828befd3
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-02-24 22:34:09 +0530

    Added solution - LeetHub

---
+++ Subset Sum Problem - GFG/subset-sum-problem.cpp
@@ -0,0 +1,98 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution
+{
+private:
+    unordered_map<int,unordered_map<int,bool>> umap;
+public:
+    bool isSubsetSum(vector<int>& arr, int target,int index=0)
+    {
+        // if(target==0)
+        // {
+        //     return true;
+        // }
+        
+        // if(index==arr.size()-1)
+        // {
+        //     return target==arr[arr.size()-1];
+        // }
+        
+        // if(umap.count(index) && umap[index].count(target))
+        // {
+        //     return umap[index][target];
+        // }
+        
+        // bool exclude=isSubsetSum(arr,target,index+1);
+        // bool include=false;
+        // if(target>=arr[index])
+        // {
+        //     include=isSubsetSum(arr,target-arr[index],index+1);
+        // }
+        
+        // // return exclude || include; //Recursive Solution TC:O(2^n) SC:O(n)
+        // return umap[index][target]=exclude || include; //Top-Down DP approach -> Recursion + Memoization 
+        // //TC:O(n*sum) SC:O(n*sum)
+        // //still TLE
+        
+        int n=arr.size();
+        
+        vector<vector<bool>> dp(n,vector<bool>(target+1,false));
+        
+        for(int i=0; i<n; i++)
+        {
+            dp[i][0]=true; //base case
+        }
+        
+        if(arr[n-1]<=target)
+        {
+            dp[n-1][arr[n-1]]=true;
+        }
+        
+        for(int i=n-2; i>=0; i--)
+        {
+            for(int j=1; j<=target; j++)
+            {
+                bool exclude=dp[i+1][j];
+                bool include=false;
+                if(j>=arr[i])
+                {
+                    include=dp[i+1][j-arr[i]];
+                }
+                
+                dp[i][j]=exclude || include;
+            }
+        }
+        
+        return dp[index][target];
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N, sum;
+        cin >> N;
+        vector<int> arr(N);
+        for(int i = 0; i < N; i++){
+            cin >> arr[i];
+        }
+        cin >> sum;
+        
+        Solution ob;
+        cout << ob.isSubsetSum(arr, sum) << endl;
+    }
+    return 0; 
+} 
+
+// } Driver Code Ends
