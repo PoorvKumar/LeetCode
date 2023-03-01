5a6aa29a3c8160f8069e52368b84c031346657ce
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-03-01 09:49:00 +0530

    Time: 128 ms (90.19%), Space: 61.2 MB (63.23%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -0,0 +1,42 @@
+class Solution 
+{
+private:
+    int partition(vector<int>& nums,int low,int high)
+    {
+        int pivot=nums[high];
+        
+        int i=low-1;
+        
+        for(int j=low; j<high; j++)
+        {
+            if(nums[j]<pivot)
+            {
+                i++;
+                swap(nums[i],nums[j]);
+            }
+        }
+        
+        swap(nums[i+1],nums[high]);
+        
+        return i+1;
+    }
+    
+    void quickSort(vector<int>& nums,int low,int high)
+    {
+        if(low<high)
+        {
+            int x=partition(nums,low,high); //partition when last index as pivot
+            quickSort(nums,low,x-1);
+            quickSort(nums,x+1,high);
+        }
+    }
+public:
+    vector<int> sortArray(vector<int>& nums)
+    {
+        sort(nums.begin(),nums.end());
+        
+        // quickSort(nums,0,nums.size()-1);
+        
+        return nums;
+    }
+};
