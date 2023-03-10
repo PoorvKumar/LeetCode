e98eb75609f9221048c037ae57349b3cd624b4e8
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-03-10 12:52:49 +0530

    Time: 3 ms (37.24%), Space: 6.6 MB (32.37%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -5,7 +5,7 @@ private:
     
     int uniquePathsUtil(int i,int j,int m,int n)
     {
-        if(i==m-1 || j==n-1)
+        if(i==m-1 || j==n-1) //base case
         {
             return 1;
         }
@@ -34,6 +34,30 @@ private:
 public:
     int uniquePaths(int m, int n) 
     {
-        return uniquePathsUtil(0,0,m,n);
+        // return uniquePathsUtil(0,0,m,n);
+        
+        vector<vector<int>> dp(m,vector<int>(n,0));
+        
+        for(int i=0; i<n; i++) //base case
+        {
+            dp[m-1][i]=1;
+        }
+        
+        for(int i=0; i<m; i++) //base case
+        {
+            dp[i][n-1]=1;
+        }
+        
+        for(int i=m-2; i>=0; i--)
+        {
+            for(int j=n-2; j>=0; j--)
+            {
+                dp[i][j]=dp[i+1][j] + dp[i][j+1]; //dp[i][j]=down + right;
+            }
+        }
+        
+        return dp[0][0]; //Bottom-UP DP approach -> Tabulation
+        //TC: O(m*n) 
+        //SC: O(m*n) + O(m*n)auxiliary space
     }
 };
