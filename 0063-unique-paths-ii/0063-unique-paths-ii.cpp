3ee1155b47ab4b24d05160aa3212c9e4615c2476
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-03-12 01:16:20 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (16.97%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -49,7 +49,9 @@ private:
         //TC: O(2^(m*n)) //as 2 calls (down , right) afor each cell , m*n cells
         //SC: O(m*n) + O(m*n)auxiliary space
         
-        return umap[i][j]=down+right;
+        return umap[i][j]=down+right; //Top-Down DP approach -> Recursion + Memoization
+        //TC: O(m*n) //as 2 calls (down , right) for each cell and answer for each cell stored , m*n cells
+        //SC: O(m*n)
     }
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) 
@@ -57,6 +59,62 @@ public:
         m=obstacleGrid.size();
         n=obstacleGrid[0].size();
         
-        return uniquePathsUtil(obstacleGrid,0,0);
+        // return uniquePathsUtil(obstacleGrid,0,0);
+        
+        vector<vector<long long int>> dp(m,vector<long long int>(n,0));
+        
+        bool flag=true;
+        
+        for(int i=n-1; i>=0; i--) //base case
+        {
+            if(obstacleGrid[m-1][i]==1)
+            {
+                flag=false;
+            }
+            
+            dp[m-1][i]=flag; //1 0
+        }
+        
+        flag=true;
+        
+        for(int i=m-1; i>=0; i--) //base case
+        {
+            if(obstacleGrid[i][n-1]==1)
+            {
+                flag=false;
+            }
+            
+            dp[i][n-1]=flag; //1 0
+        }
+        
+        // for(auto x:dp)
+        // {
+        //     for(auto y:x)
+        //     {
+        //         cout<<y<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        
+        for(int i=m-2; i>=0; i--)
+        {
+            for(int j=n-2; j>=0; j--)
+            {
+                // if(obstacleGrid[i][j]==1)
+                // {
+                //     dp[i][j]=0;
+                // }
+                // else
+                // {
+                //     dp[i][j]=dp[i+1][j] + dp[i][j+1]; //dp[i][j]=down+right;
+                // }
+                
+                dp[i][j]=obstacleGrid[i][j]==1?0:dp[i+1][j]+dp[i][j+1]; //dp[i][j]=down+right;
+            }
+        }
+        
+        return dp[0][0]; //Bottom-Up DP approach -> Tabulation
+        //TC: O(m*n)
+        //SC: O(m*n) + O(m*n)auxiliary space
     }
 };
