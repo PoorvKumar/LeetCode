63160e56092f496de4d59b6b856aa21b1a13539d
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-03-14 16:58:32 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (13.84%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -4,34 +4,34 @@ private:
     vector<vector<int>> res;
     vector<int> vec;
     
-//     void combinationSum3Util(int n,int val)
-//     {
-//         if(n==0)
-//         {
-//             res.push_back(vec);
-//             return ;
-//         }
+    void combinationSum3Util1(int n,int val)
+    {
+        if(n==0)
+        {
+            res.push_back(vec);
+            return ;
+        }
         
-//         if(val>9)
-//         {
-//             return ;
-//         }
+        if(val>9)
+        {
+            return ;
+        }
         
-//         if(n>=val)
-//         {
-//             //exclude
-//             combinationSum3Util(n,val+1);
+        if(n>=val)
+        {
+            //exclude
+            combinationSum3Util1(n,val+1);
             
-//             //include
-//             vec.push_back(val);
-//             combinationSum3Util(n-val,val+1);
-//             vec.pop_back();
-//         }
+            //include
+            vec.push_back(val);
+            combinationSum3Util1(n-val,val+1);
+            vec.pop_back();
+        }
         
-//         return ;
-//     }
+        return ;
+    }
     
-    void combinationSum3Util(int n,int val,int k)
+    void combinationSum3Util2(int n,int val,int k)
     {
         if(n==0)
         {
@@ -50,11 +50,11 @@ private:
         if(n>=val) //as val always increasing
         {
             //exclude
-            combinationSum3Util(n,val+1,k);
+            combinationSum3Util2(n,val+1,k);
             
             //include
             vec.push_back(val);
-            combinationSum3Util(n-val,val+1,k-1);
+            combinationSum3Util2(n-val,val+1,k-1);
             vec.pop_back();
         }
         
@@ -63,8 +63,22 @@ private:
 public:
     vector<vector<int>> combinationSum3(int k, int n)
     {
-        combinationSum3Util(n,1,k);
+//         combinationSum3Util2(n,1,k);
         
-        return res;
+//         return res;
+        
+        combinationSum3Util1(n,1);
+        
+        vector<vector<int>> ans;
+        
+        for(auto &x:res)
+        {
+            if(x.size()==k)
+            {
+                ans.push_back(x);
+            }
+        }
+        
+        return ans;
     }
 };
