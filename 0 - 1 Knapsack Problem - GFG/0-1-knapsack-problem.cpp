95b0a666e74212adc91544bce9f97a76df7443df
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-03-15 19:40:44 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -8,7 +8,7 @@ class Solution
 {
 private:
     int n;
-    unordered_map<int,unordered_map<int,int>> umap;
+    unordered_map<int,unordered_map<int,int>> umap; //umap[index][W]
     
     int knapSackUtil(int wt[],int val[],int index,int W)
     {
@@ -43,7 +43,9 @@ private:
         //TC: O(2^n) //as 2 calls(include,exclude) for every index ,n index
         //SC: O(n) + O(n) auxiliary space
         
-        return umap[index][W]=max(exclude,include);
+        return umap[index][W]=max(exclude,include); //Top-Down DP approach -> Recursion + Memoization
+        //TC: O(n*W) //as for every index for every W Recursive calls solution memoized
+        //SC: O(n*W)
     }
 public:
     //Function to return max value that can be put in knapsack of capacity W.
@@ -51,7 +53,43 @@ public:
     {
         this->n=n;
         
-        return knapSackUtil(wt,val,0,W);
+        // return knapSackUtil(wt,val,0,W);
+        
+        vector<vector<int>> dp(n,vector<int>(W+1,0)); //dp[index][W]
+        
+        for(int i=0; i<n; i++) //base case
+        {
+            dp[i][0]=0;
+        }
+        
+        for(int i=0; i<=W; i++) //base case
+        {
+            if(i>=wt[n-1])
+            {
+                dp[n-1][i]=val[n-1];
+            }
+            else
+            {
+                dp[n-1][i]=0;
+            }
+        }
+        
+        for(int i=n-2; i>=0; i--)
+        {
+            for(int j=0; j<=W; j++)
+            {
+                int exclude=dp[i+1][j];
+                int include=0;
+                if(j>=wt[i])
+                {
+                    include=val[i]+dp[i+1][j-wt[i]];
+                }
+                
+                dp[i][j]=max(exclude,include);
+            }
+        }
+        
+        return dp[0][W]; //dp[index][W];
     }
 };
 
