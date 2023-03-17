19f81c62cd90e6c5f1cf943dfee0b64996bd475a
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-03-17 23:19:15 +0530

    Time: 302 ms (38.12%), Space: 61.3 MB (11.91%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,33 +1,35 @@
 class Solution 
 {
 private:
+    int sum=0;
     unordered_map<int,unordered_map<int,int>> umap;
     
-    int targetSumUtil(vector<int>& nums,int index,int target)
+    int targetSumUtil(vector<int>& nums,int index,int plus,int target)
     {
         if(index==nums.size())
         {
-            // return target==0?1:0;
-            return target==0;
+            return 2*plus-sum==target; //plus+minus==sum && plus-minus==target must be true plus is a subset of nums
         }
         
-        if(umap.count(index) && umap[index].count(target))
+        if(umap.count(index) && umap[index].count(plus))
         {
-            return umap[index][target];
+            return umap[index][plus];
         }
         
-        int plus=targetSumUtil(nums,index+1,target-nums[index]);
-        int minus=targetSumUtil(nums,index+1,target+nums[index]);
+        int exclude=targetSumUtil(nums,index+1,plus,target);
+        int include=targetSumUtil(nums,index+1,plus+nums[index],target);
         
-        // return plus+minus; //recursive Solution
-        //TC: O(2^n) //as 2 calls(plus,minus) for every index , n index
+        // return exclude+include; //Recursive Solution
+        //TC: O(2^n) //as 2 (include,exclude) calls at each index
         //SC: O(n) + O(n)auxiliary space
         
-        return umap[index][target]=plus+minus;
+        return umap[index][plus]=exclude+include;
     }
 public:
     int findTargetSumWays(vector<int>& nums, int target) 
     {
-        return targetSumUtil(nums,0,target);
+        sum=accumulate(nums.begin(),nums.end(),0);
+        
+        return targetSumUtil(nums,0,0,target);
     }
 };
