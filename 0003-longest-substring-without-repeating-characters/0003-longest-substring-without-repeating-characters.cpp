016c710663e1eafa23ae881b57461773b348c11c
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-04-01 12:43:45 +0530

    Time: 35 ms (31.11%), Space: 10.9 MB (27.51%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -4,38 +4,74 @@ public:
     int lengthOfLongestSubstring(string s) 
     {
         int ans=0;
-        int low=0;
-        unordered_map<char,int> umap;
+        int left=0;
+        int right=0;
+        unordered_set<char> uset;
         
-        if(s.length()==1)
+        while(right<s.length())
         {
-            return 1;
-        }
-        
-        for(int i=0; i<s.length(); i++)
-        {
-            if(umap.count(s[i]) && umap[s[i]]!=-1)
+            if(uset.count(s[right]))
             {
-                ans=max(ans,i-low);
-                low=umap[s[i]]+1; //previous+1
-                umap[s[i]]=i; //current
-                for(auto &x:umap)
+                while(left<right)
                 {
-                    if(x.second<low)
+                    uset.erase(s[left]);
+                    if(s[left]==s[right])
                     {
-                        x.second=-1;
+                        left++;
+                        break;
                     }
+                    left++;
                 }
             }
-            else
-            {
-                umap[s[i]]=i;
-            }
+            uset.insert(s[right]);
+            ans=max(ans,right-left+1);
+            right++;
         }
         
-        int n=s.length(); //does not work when s.length() instead of n 
-        ans=max(ans,n-low); //incase of no repeating characters
-        
-        return ans;
+        return ans; //Two Pointers Approach
+        //TC: O(2*n) //as O(n) for left O(n) for right
+        //SC: O(n)
     }
 };
+
+// class Solution 
+// {
+// public:
+//     int lengthOfLongestSubstring(string s)
+//     {
+//         int ans=0;
+//         int low=0;
+//         unordered_map<char,int> umap;
+        
+//         if(s.length()==1)
+//         {
+//             return 1;
+//         }
+        
+//         for(int i=0; i<s.length(); i++)
+//         {
+//             if(umap.count(s[i]) && umap[s[i]]!=-1)
+//             {
+//                 ans=max(ans,i-low);
+//                 low=umap[s[i]]+1; //previous+1
+//                 umap[s[i]]=i; //current
+//                 for(auto &x:umap)
+//                 {
+//                     if(x.second<low)
+//                     {
+//                         x.second=-1;
+//                     }
+//                 }
+//             }
+//             else
+//             {
+//                 umap[s[i]]=i;
+//             }
+//         }
+        
+//         int n=s.length(); //does not work when s.length() instead of n 
+//         ans=max(ans,n-low); //incase of no repeating characters
+        
+//         return ans; //TC: O(n^2) optmising Brute Force
+//     }
+// };
