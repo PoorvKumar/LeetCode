85a5f94780a8a537f8253632955122965422dc36
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-04-01 17:43:03 +0530

    Time: 23 ms (53.03%), Space: 8.4 MB (64.79%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -25,6 +25,44 @@ public:
         int n=s.length();
         ans=max(ans,n-low); //when i becomes equal to n
         
-        return ans;
+        return ans; //Two pointers Approach -> Optimised 
+        //TC: O(n)
+        //SC: O(n)
     }
 };
+
+// class Solution 
+// {
+// public:
+//     int lengthOfLongestSubstring(string s) 
+//     {
+//         int ans=0;
+//         int left=0;
+//         int right=0;
+//         unordered_set<char> uset;
+        
+//         while(right<s.length())
+//         {
+//             if(uset.count(s[right]))
+//             {
+//                 while(left<right)
+//                 {
+//                     uset.erase(s[left]);
+//                     if(s[left]==s[right])
+//                     {
+//                         left++;
+//                         break;
+//                     }
+//                     left++;
+//                 }
+//             }
+//             uset.insert(s[right]);
+//             ans=max(ans,right-left+1);
+//             right++;
+//         }
+        
+//         return ans; //Two Pointers Approach
+//         //TC: O(2*n) //as O(n) for left O(n) for right
+//         //SC: O(n)
+//     }
+// };
