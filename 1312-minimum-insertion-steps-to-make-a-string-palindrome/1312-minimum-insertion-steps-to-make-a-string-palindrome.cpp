2acd83459c145f320fcc6bff6ca82ef4e4c0d959
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-04-08 16:38:12 +0530

    Time: 52 ms (75.35%), Space: 6.7 MB (98.01%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -25,7 +25,7 @@ private:
         int t2=longestCommonUtil(s1,s2,index1,index2+1);
         
         // return max(t1,t2); //Recursive Solution
-        //TC: O(2^(n1+n2)) // as 2 ()t1,t2) calls for every index of s1 for every index of s2
+        //TC: O(2^(n1+n2)) // as 2 (t1,t2) calls for every index of s1 for every index of s2
         //SC: O(n1*n2)+O(n1+n2)auxiliary stack space
         
         return umap[index1][index2]=max(t1,t2); //Top-Down DP approach -> Recursion + Memoization
@@ -43,8 +43,28 @@ public:
         
         int n=s.length();
         
-        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
-        //dp[index1][index2]
+//         vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+//         //dp[index1][index2]
+        
+//         for(int i=n-1; i>=0; i--)
+//         {
+//             for(int j=n-1; j>=0; j--)
+//             {
+//                 if(s1[i]==s2[j])
+//                 {
+//                     dp[i][j]=1+dp[i+1][j+1];
+//                     continue;
+//                 }
+//                 dp[i][j]=max(dp[i+1][j],dp[i][j+1]);
+//             }
+//         }
+        
+//         return s.length()-dp[0][0]; //Botom-Up DP approach -> Tabulation
+//         //TC: O(n1*n2)
+//         //SC: O(n1*n2)
+        
+        vector<int> prev(n+1,0);
+        vector<int> curr(n+1,0);
         
         for(int i=n-1; i>=0; i--)
         {
@@ -52,15 +72,18 @@ public:
             {
                 if(s1[i]==s2[j])
                 {
-                    dp[i][j]=1+dp[i+1][j+1];
+                    curr[j]=1+prev[j+1];
                     continue;
                 }
-                dp[i][j]=max(dp[i+1][j],dp[i][j+1]);
+                curr[j]=max(prev[j],curr[j+1]);
             }
+            prev=curr;
         }
         
-        return s.length()-dp[0][0]; //Botom-Up DP approach -> Tabulation
+        return s.length()-prev[0]; //Space Optimisation
         //TC: O(n1*n2)
-        //SC: O(n1*n2)
+        //SC: O(n2)
+        
+        //n1==n2 && n==n1 is true
     }
 };
