18e3f45af6e3e13f94f32b727d7be1964aed0ffd
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-04-08 16:19:55 +0530

    Time: 1838 ms (5.28%), Space: 256.7 MB (5.73%) - LeetHub

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,44 @@
+class Solution 
+{
+private:
+    unordered_map<int,unordered_map<int,int>> umap; //umap[index1][index2]
+    
+    int longestCommonUtil(string& s1,string& s2,int index1,int index2)
+    {
+        if(index1>=s1.length() || index2>=s2.length()) //base case
+        {
+            return 0;
+        }
+        
+        if(umap.count(index1) && umap[index1].count(index2))
+        {
+            return umap[index1][index2];
+        }
+        
+        if(s1[index1]==s2[index2])
+        {
+            // return 1+longestCommonUtil(s1,s2,index1+1,index2+1);
+            return umap[index1][index2]=1+longestCommonUtil(s1,s2,index1+1,index2+1);
+        }
+        
+        int t1=longestCommonUtil(s1,s2,index1+1,index2);
+        int t2=longestCommonUtil(s1,s2,index1,index2+1);
+        
+        // return max(t1,t2); //Recursive Solution
+        //TC: O(2^(n1+n2)) // as 2 ()t1,t2) calls for every index of s1 for every index of s2
+        //SC: O(n1*n2)+O(n1+n2)auxiliary stack space
+        
+        return umap[index1][index2]=max(t1,t2); //Top-Down DP approach -> Recursion + Memoization
+        //TC: O(n1*n2) //as for every index in s1 for every index in s2 Recursion calls Memoized
+        //SC: O(n1*n2)+O(n1+n2)auxiliary stack space
+    }
+public:
+    int minInsertions(string s) 
+    {
+        string s1=s;
+        reverse(s.begin(),s.end());
+        string s2=s;
+        
+        return s.length()-longestCommonUtil(s1,s2,0,0);
+    }
+};
