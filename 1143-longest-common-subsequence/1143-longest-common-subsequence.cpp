55458ed1befea424cce5915b406cc277d63614b1
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-04-08 10:44:18 +0530

    Time: 78 ms (26.25%), Space: 24.5 MB (32.38%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,7 +1,7 @@
 class Solution 
 {
 private:
-    unordered_map<int,unordered_map<int,int>> umap;
+    unordered_map<int,unordered_map<int,int>> umap; //umap[x][y]
     
     int longestCommonUtil(string& text1,string& text2,int x,int y)
     {
@@ -44,7 +44,7 @@ private:
         
         if(text1[x]==text2[y])
         {
-            return umap[x][y]= 1+longestCommonUtil(text1,text2,x+1,y+1);
+            return umap[x][y]=1+longestCommonUtil(text1,text2,x+1,y+1);
         }
         
         int t1=longestCommonUtil(text1,text2,x+1,y);
@@ -59,6 +59,45 @@ private:
 public:
     int longestCommonSubsequence(string text1, string text2) 
     {
-        return longestCommonUtil(text1,text2,0,0);
+        // return longestCommonUtil(text1,text2,0,0);
+        
+        if(text1.length()<text2.length())
+        {
+            text1.swap(text2);
+        }
+        
+        int n1=text1.length();
+        int n2=text2.length();
+        
+        vector<vector<int>> dp(n1+1,vector<int>(n2+1,0)); //dp[x][y]
+        
+        for(int i=0; i<n2; i++) //base case
+        {
+            dp[n1-1][i]=text1[n1-1]==text2[i];
+        }
+        
+        for(int i=0; i<n1; i++) //base case
+        {
+            dp[i][n2-1]=text1[i]==text2[n2-1];
+        }
+        
+        for(int i=n1-1; i>=0; i--)
+        {
+            for(int j=n2-1; j>=0; j--)
+            {
+                if(text1[i]==text2[j])
+                {
+                    dp[i][j]=1+dp[i+1][j+1];
+                    continue;
+                }
+                
+                int t1=dp[i+1][j];
+                int t2=dp[i][j+1];
+                
+                dp[i][j]=max(t1,t2);
+            }
+        }
+        
+        return dp[0][0];
     }
 };
