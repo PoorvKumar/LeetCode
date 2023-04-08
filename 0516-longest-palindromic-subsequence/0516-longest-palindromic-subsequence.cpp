8cbddec1155a4464ad8248ee4ee0ad4f99d2ad25
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-04-08 13:48:32 +0530

    Time: 1943 ms (5.06%), Space: 275.1 MB (5.32%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -39,5 +39,27 @@ public:
         string s2=s;
         
         return longestCommonUtil(s1,s2,0,0);
+        
+        int n=s.length();
+        
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        //dp[index1][index2]
+        
+        for(int i=n-1; i>=0; i--)
+        {
+            for(int j=n-1; j>=0; j--)
+            {
+                if(s1[i]==s2[j])
+                {
+                    dp[i][j]=1+dp[i+1][j+1];
+                    continue;
+                }
+                dp[i][j]=max(dp[i+1][j],dp[i][j+1]);
+            }
+        }
+        
+        return dp[0][0]; //Bottom-Up DP approach -> Tabulation
+        //TC: O(n1*n2)
+        //SC: O(n1*n2)
     }
 };
