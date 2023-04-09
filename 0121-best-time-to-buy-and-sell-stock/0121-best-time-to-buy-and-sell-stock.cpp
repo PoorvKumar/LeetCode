cc9091dab12398a6c8eaed3283221a2b679c5d7d
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-04-09 11:25:44 +0530

    Time: 140 ms (48.24%), Space: 93.5 MB (11.37%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,31 +1,53 @@
 class Solution 
 {
 public:
-    int maxProfit(vector<int>& prices) 
+    //Those who cannot Remember the Past are Condemned to Repeat it
+    //                                                         ~ DP
+    
+    int maxProfit(vector<int>& prices)
     {
-        int start=0;
-        int end=0;
+        int minim=prices[0];
         int maxProfit=0;
-        
-        start=end=prices[0];
         
         for(int i=1; i<prices.size(); i++)
         {
-            if(prices[i]<=end)
-            {
-                // start=prices[i];
-                start=min(start,prices[i]);
-                end=prices[i];
-            }
-            else
-            {
-                // end=prices[i];
-                end=max(end,prices[i]);
-            }
-            
-            maxProfit=max(maxProfit,end-start);
+            int cost=prices[i]-minim;
+            maxProfit=max(maxProfit,cost);
+            minim=min(minim,prices[i]);
         }
         
         return maxProfit;
+        //TC: O(n)
+        //SC: O(1)
     }
+    
+    //this is a DP Problem as remebering the Past minimum price
+    
+//     int maxProfit(vector<int>& prices) 
+//     {
+//         int start=0;
+//         int end=0;
+//         int maxProfit=0;
+        
+//         start=end=prices[0];
+        
+//         for(int i=1; i<prices.size(); i++)
+//         {
+//             if(prices[i]<=end)
+//             {
+//                 // start=prices[i];
+//                 start=min(start,prices[i]);
+//                 end=prices[i];
+//             }
+//             else
+//             {
+//                 // end=prices[i];
+//                 end=max(end,prices[i]);
+//             }
+            
+//             maxProfit=max(maxProfit,end-start);
+//         }
+        
+//         return maxProfit;
+//     }
 };
