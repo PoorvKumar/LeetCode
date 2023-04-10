33db4d4c7964dde100effa2ee33884cf41312b89
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-04-10 16:42:16 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (56.25%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,29 @@
+class Solution 
+{
+public:
+    bool isValid(string s) 
+    {
+        
+        
+        stack<char> stk;
+        
+        for(auto c:s)
+        {
+            if(c=='(' || c=='[' || c=='{')
+            {
+                stk.push(c);
+            }
+            else
+            {
+                if(!stk.empty() && (c==')' && stk.top()=='(' || c==']' && stk.top()=='[' || c=='}' && stk.top()=='{'))
+                {
+                    stk.pop();
+                    continue;
+                }
+                return false;
+            }
+        }
+        
+        return stk.empty();
+    }
+};
