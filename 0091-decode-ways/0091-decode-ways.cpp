63bc2d59ea8e5536225a8bb6034e5e547a06cca7
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-04-11 17:58:19 +0530

    Time: 2 ms (57.21%), Space: 6.3 MB (51.55%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -47,15 +47,77 @@ private:
         }
         
         ans=ans+numDecodingsUtil(s,index+1);
-        // return ans;
+        // return ans; //Recursive Solution
+        //TC: O(2^n) //as two (two_digit,one_digit) calls for every index
+        //SC: O(n)+O(n)auxiliary stack space
         
-        return umap[index]=ans;
+        return umap[index]=ans; //Top-Down DP apporach -> Recursion + Memoization
+        //TC: O(n) //as for every index Recursion calls Memoized
+        //SC: O(n)+O(n)auxiliary stack space
     }
 public:
     int numDecodings(string s) 
     {
-        int ans=numDecodingsUtil(s,0);
+//         int ans=numDecodingsUtil(s,0);
         
-        return ans>=0?ans:0;
+//         return ans>=0?ans:0;
+        
+        int n=s.length();
+        
+        vector<int> dp(n+1,0);
+        //dp[index]
+        
+        // for(int i=0; i<n; i++) //base case
+        // {
+        //     if(s[i]=='0')
+        //     {
+        //         dp[i]=INT_MIN;
+        //     }
+        // }
+        
+        dp[n]=1; //base case
+        
+        for(int i=n-1; i>=0; i--)
+        {
+            if(s[i]=='0')
+            {
+                dp[i]=INT_MIN;
+                continue;
+            }
+            
+            int ans=0;
+            if(i<n-1)
+            {
+                string num=s.substr(i,2);
+                int number=stoi(num);
+                
+                if(s[i+1]=='0')
+                {
+                    if(number>26)
+                    {
+                        dp[i]=0;
+                        continue;
+                    }
+                    dp[i]=dp[i+2];
+                    continue;
+                }
+                
+                if(number<=26)
+                {
+                    ans=dp[i+2];
+                    if(ans<0)
+                    {
+                        ans=0;
+                    }
+                }
+            }
+            
+            ans=ans+dp[i+1];
+            
+            dp[i]=ans;
+        }
+        
+        int res=dp[0];
+        return res>=0?res:0;
     }
 };
