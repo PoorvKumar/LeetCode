854405bb167753f35bfe76e5f2f286d77db40cae
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-04-27 16:26:35 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (63.99%) - LeetHub

--- 0319-bulb-switcher/0319-bulb-switcher.cpp
+++ 0319-bulb-switcher/0319-bulb-switcher.cpp
@@ -1,9 +1,46 @@
 class Solution 
 {
+private:
+    int sqrtFun(int n)
+    {
+        int low=1;
+        int high=n;
+        int mid=(low+high)/2;
+        
+        while(low<high)
+        {
+            if(mid*mid==n)
+            {
+                return mid;
+            }
+            else if(mid*mid<n)
+            {
+                low=mid+1;
+            }
+            else
+            {
+                high=mid;
+            }
+            mid=(low+high)/2;
+        }
+        
+        return mid-1;
+    }
 public:
     int bulbSwitch(int n) 
     {
+//         if(n==0)
+//         {
+//             return n;
+//         }
+        
+//         if(n<=3)
+//         {
+//             return 1;
+//         }
+        
         int ans=(int)sqrt(n);
+        // int ans=sqrtFun(n);
         
         return ans;
     }
