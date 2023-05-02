36eccd01c2562fefa8344299e0bd5bbc2f6d8d03
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-05-02 13:50:52 +0530

    Time: 4 ms (47.85%), Space: 7.8 MB (46.40%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -3,42 +3,69 @@ class Solution
 private:
     vector<vector<int>> res;
     
-    void permuteUtil(unordered_set<int>& uset,vector<int>& vec)
+    void permuteUtil(vector<int>& nums,int index)
     {
-        if(uset.empty())
+        if(index==nums.size()-1)
         {
-            res.push_back(vec);
+            res.push_back(nums);
             return ;
         }
         
-        unordered_set<int> uset1=uset;
-        
-        for(auto x:uset1)
+        for(int i=index; i<nums.size(); i++)
         {
-            vec.push_back(x);
-            uset.erase(x);
-            permuteUtil(uset,vec);
-            uset.insert(x);
-            vec.pop_back();
+            swap(nums[index],nums[i]);
+            permuteUtil(nums,index+1);
+            swap(nums[index],nums[i]);
         }
         
-        return ;
+        return ; 
+        //TC: O(n*n!)
+        ///SC: O(n) auxilairy stack space (depth of recursion -> O(n))
+        
     }
+    
+//     void permuteUtil(unordered_set<int>& uset,vector<int>& vec)
+//     {
+//         if(uset.empty())
+//         {
+//             res.push_back(vec);
+//             return ;
+//         }
+        
+//         unordered_set<int> uset1=uset;
+        
+//         for(auto x:uset1)
+//         {
+//             vec.push_back(x);
+//             uset.erase(x);
+//             permuteUtil(uset,vec);
+//             uset.insert(x);
+//             vec.pop_back();
+//         }
+        
+//         return ;
+//         //TC: O(n*n!)
+//         //SC: O(n)+O(n) //depth of recursion //unordered_set<int> uset;
+//     }
     
 public:
     vector<vector<int>> permute(vector<int>& nums) 
     {
-        unordered_set<int> uset;
-        vector<int> vec;
-        
-        for(auto x:nums)
-        {
-            uset.insert(x);
-        }
-        
-        permuteUtil(uset,vec);
+        permuteUtil(nums,0);
         
         return res;
+        
+//         unordered_set<int> uset;
+//         vector<int> vec;
+        
+//         for(auto x:nums)
+//         {
+//             uset.insert(x);
+//         }
+        
+//         permuteUtil(uset,vec);
+        
+//         return res;
         
 //         int fact=factorial(nums.size());
         
