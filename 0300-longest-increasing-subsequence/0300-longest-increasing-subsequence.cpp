31e2e82ee3da5f80a5237b3311ca36cc4b65fd6c
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-05-08 15:22:21 +0530

    Time: 613 ms (31.15%), Space: 10.7 MB (35.85%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -43,56 +43,58 @@ public:
         // return lengthOfLISUtil(nums,0,0); //getting TLE
         
         int n=nums.size();
-        vector<vector<int>> dp(n+1,vector<int>(n+1,0)); //dp[index][prev]
+//         vector<vector<int>> dp(n+1,vector<int>(n+1,0)); //dp[index][prev]
         
-        //shifting of index 0->1
-        
-        for(int i=n-1; i>=0; i--) //index
-        {
-            for(int j=n; j>=0; j--) //prev -> will be always less than or equal to i+1
-            {
-                int exclude=dp[i+1][j];
-                int include=0;
-                
-                if(j==0 || nums[i]>nums[j-1])
-                {
-                    // int k=i+1;
-                    // while(k<nums.size() && nums[i]>=nums[k])
-                    // {
-                    //     k++;
-                    // }
-                    
-                    include=1+dp[i+1][i+1];
-                }
-                
-                dp[i][j]=max(exclude,include);
-            }
-        }
-        
-        return dp[0][0]; //Bottom-Up DP approach -> Tabulation
-        //TC: O(n*n)
-        //SC: O(n*n)
-        
-//         vector<int> curr(n+1,0);
-//         vector<int> prev(n+1,0);
+//         //shifting of index 0->1
         
 //         for(int i=n-1; i>=0; i--) //index
 //         {
-//             for(int j=i+1; j>=0; j--) //prev
+//             for(int j=n; j>=0; j--) //prev -> will be always less than or equal to i+1
 //             {
-//                 int exclude=prev[j];
+//                 int exclude=dp[i+1][j];
 //                 int include=0;
                 
 //                 if(j==0 || nums[i]>nums[j-1])
 //                 {
-//                     include=1+prev[i+1];
+//                     // int k=i+1;
+//                     // while(k<nums.size() && nums[i]>=nums[k])
+//                     // {
+//                     //     k++;
+//                     // }
+                    
+//                     include=1+dp[i+1][i+1];
 //                 }
                 
-//                 curr[j]=max(exclude,include);
+//                 dp[i][j]=max(exclude,include);
 //             }
-//             prev=curr;
 //         }
         
-//         return prev[0];
+//         return dp[0][0]; //Bottom-Up DP approach -> Tabulation
+//         //TC: O(n*n)
+//         //SC: O(n*n)
+        
+        vector<int> curr(n+1,0);
+        vector<int> prev(n+1,0);
+        
+        for(int i=n-1; i>=0; i--) //index
+        {
+            for(int j=i+1; j>=0; j--) //prev
+            {
+                int exclude=prev[j];
+                int include=0;
+                
+                if(j==0 || nums[i]>nums[j-1])
+                {
+                    include=1+prev[i+1];
+                }
+                
+                curr[j]=max(exclude,include);
+            }
+            prev=curr;
+        }
+        
+        return prev[0]; //Space Optimisation
+        //TC: O(n*n)
+        //SC: O(n)*2
     }
 };
