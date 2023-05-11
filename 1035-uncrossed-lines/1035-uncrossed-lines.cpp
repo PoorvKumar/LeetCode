b6cfa68d8e17f07147451fbbb80a4960a99eba0b
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-05-11 12:34:42 +0530

    Time: 839 ms (5.10%), Space: 12.9 MB (63.96%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.cpp
+++ 1035-uncrossed-lines/1035-uncrossed-lines.cpp
@@ -51,12 +51,59 @@ private:
         //SC: O(m+n)+O(m+n)auxiliary stack space
         
         return umap[i][j]=ans; //Top-Down DP approach -> Recursion + Memoization
-        //TC: O(m*n) //as for every i for evry j Recursion calls Memoized
+        //TC: O(m*n) //as for every i for every j Recursion calls Memoized
         //SC: O(m*n)+O(m+n)auxiliary stack space
     }
 public:
     int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) 
     {
-        return maxUncrossedUtil(nums1,nums2,0,0);
+        // return maxUncrossedUtil(nums1,nums2,0,0);
+        
+        int m=nums1.size();
+        int n=nums2.size();
+        
+        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+        //dp[i][j]
+        
+        for(int i=m-1; i>=0; i--)
+        {
+            for(int j=n-1; j>=0; j--)
+            {
+                int notConnect1=dp[i+1][j];
+                int notConnect2=dp[i][j+1];
+                
+                int k=j;
+                while(k<nums2.size() && nums1[i]!=nums2[k])
+                {
+                    k++;
+                }
+                
+                int connect1=k<nums2.size()?dp[i+1][k+1]:dp[i+1][k];
+                if(k<nums2.size() && nums1[i]==nums2[k])
+                {
+                    connect1=connect1+1;
+                }
+                
+                k=i;
+                while(k<nums1.size() && nums2[j]!=nums1[k])
+                {
+                    k++;
+                }
+                
+                int connect2=k<nums1.size()?dp[k+1][j+1]:dp[k][j+1];
+                if(k<nums1.size() && nums2[j]==nums1[k])
+                {
+                    connect2=connect2+1;
+                }
+                
+                int ans=max(notConnect1,notConnect2);
+                ans=max(ans,connect1);
+                ans=max(ans,connect2);
+                
+                dp[i][j]=ans;
+            }
+        }
+        
+        return dp[0][0];
     }
 };
