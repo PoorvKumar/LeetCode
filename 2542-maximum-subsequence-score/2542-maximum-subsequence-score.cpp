87acab564d8516eaed82a22fabd3bcd55787dbee
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-05-24 20:44:12 +0530

    Time: 271 ms (72.17%), Space: 91.9 MB (60.84%) - LeetHub

--- 2542-maximum-subsequence-score/2542-maximum-subsequence-score.cpp
+++ 2542-maximum-subsequence-score/2542-maximum-subsequence-score.cpp
@@ -12,27 +12,7 @@ public:
             vec.push_back(make_pair(nums2[i],nums1[i]));
         }
         
-        sort(vec.begin(),vec.end()); //O(nlogn)
-        
-        // priority_queue<int> pq; //here instead of max heap we can implement min heap to remove the minimum element and find sum in less time
-        
-//         for(int i=n-1; i>n-k; i--)
-//         {
-//             pq.push(vec[i].second);
-//         }
-        
-//         long long ans=0;
-        
-//         for(int i=n-k; i>=0; i--)
-//         {
-//             long long sum=vec[i].second;
-//             // sum=sum+topKElem(pq,k); 
-            
-//             long long val=sum*vec[i].first;   
-//             pq.push(vec[i].second); //O(logn)
-            
-//             ans=max(ans,val);
-//         }
+        sort(vec.begin(),vec.end());
         
         priority_queue<int,vector<int>,greater<int>> pq; //MIN HEAP
         
