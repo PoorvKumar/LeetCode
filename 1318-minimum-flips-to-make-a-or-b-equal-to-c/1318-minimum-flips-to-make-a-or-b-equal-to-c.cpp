52ca5d7f02b3acd1b57aaebff2cf54fd3cd2d1c5
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-06-07 14:34:22 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (53.28%) - LeetHub

--- 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -6,31 +6,53 @@ public:
         bitset<32> bsA(a);
         bitset<32> bsB(b);
         
-        string s0=bsA.to_string();
-        string s1=bsB.to_string();
+        // string s0=bsA.to_string();
+        // string s1=bsB.to_string();
         
-        string s2=bitset<32>(c).to_string(); //declared an object of type class bitset<32> by calling the parameterised constructor of class bitset and then called to_string() function over the object of rtype class bitset<32>
+        // string s2=bitset<32>(c).to_string(); //declared an object of type class bitset<32> by calling the parameterised constructor of class bitset and then called to_string() function over the object of rtype class bitset<32>
         
         bitset<32> bsC(c);
         
         int ans=0;
         
+        // for(int i=0; i<32; i++)
+        // {
+        //     if(s2[i]=='0')
+        //     {
+        //         if(s0[i]=='1')
+        //         {
+        //             ans++;
+        //         }
+        //         if(s1[i]=='1')
+        //         {
+        //             ans++;
+        //         }
+        //     }
+        //     else
+        //     {
+        //         if(s0[i]=='0' && s1[i]=='0')
+        //         {
+        //             ans++;
+        //         }
+        //     }
+        // }
+        
         for(int i=0; i<32; i++)
         {
-            if(s2[i]=='0')
+            if(bsC.test(i)) //true if bit at index i in object bs2 of type class bitset set to 1
             {
-                if(s0[i]=='1')
+                if(bsA[i]==0 && bsB[i]==0) 
                 {
-                    ans++;
-                }
-                if(s1[i]=='1')
-                {
-                    ans++;
+                    ans++; //[](subscript) operator overloaded, operator[](), in class bitset
                 }
             }
             else
             {
-                if(s0[i]=='0' && s1[i]=='0')
+                if(bsA[i]==1)
+                {
+                    ans++;
+                }
+                if(bsB[i]==1)
                 {
                     ans++;
                 }
