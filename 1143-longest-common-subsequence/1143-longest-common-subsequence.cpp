34d6b847179d4ce28b32ac1297a714055974364b
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-06-18 11:36:29 +0530

    Time: 69 ms (54.79%), Space: 24.4 MB (63.20%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -40,6 +40,37 @@ private:
 public:
     int longestCommonSubsequence(string text1, string text2) 
     {
-        return lcsUtil(text1,text2,0,0);
+        // return lcsUtil(text1,text2,0,0);
+        
+        int m=text1.length();
+        int n=text2.length();
+        
+        vector<vector<int>> dp(m+1,vector<int>(n+1,0)); //dp[i][j]
+        
+        for(int i=m-1; i>=0; i--)
+        {
+            for(int j=n-1; j>=0; j--)
+            {
+                int ans=0;
+                
+                if(text1[i]==text2[j])
+                {
+                    ans=1+dp[i+1][j+1];
+                }
+                else
+                {
+                    int t1=dp[i+1][j];
+                    int t2=dp[i][j+1];
+                    
+                    ans=max(t1,t2);
+                }
+                
+                dp[i][j]=ans;
+            }
+        }
+        
+        return dp[0][0]; //Bottom-Up DP approach -> Tabulation
+        //TC: O(m*n)
+        //SC: O(m*n)
     }
 };
