5cca89c4ffb1c290514af86177db0a5c01bd9b98
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-06-19 10:44:03 +0530

    Time: 3 ms (47.50%), Space: 8.2 MB (44.36%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -7,15 +7,15 @@ public:
         
         for(int i=0; i<nums.size(); i++) //Selection Sort
         {
-            // int minimIndex=i;
-            // for(int j=i+1; j<nums.size(); j++)
-            // {
-            //     if(nums[j]<nums[minimIndex])
-            //     {
-            //         minimIndex=j;
-            //     }
-            // }
-            int minimIndex=min_element(begin(nums)+i,end(nums))-nums.begin();
+            int minimIndex=i;
+            for(int j=i+1; j<nums.size(); j++)
+            {
+                if(nums[j]<nums[minimIndex])
+                {
+                    minimIndex=j;
+                }
+            }
+            // int minimIndex=min_element(begin(nums)+i,end(nums))-nums.begin();
             
             swap(nums[i],nums[minimIndex]);
         }
