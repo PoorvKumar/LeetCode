65e3b99733804397ee04a4ee29e7e15995b1893d
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-06-25 19:00:44 +0530

    Time: 3 ms (35.22%), Space: 6.2 MB (36.95%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -25,6 +25,24 @@ private:
 public:
     int climbStairs(int n) 
     {
-        return climbStairsUtil(n);
+        // return climbStairsUtil(n);
+        
+        if(n<=2)
+        {
+            return n;
+        }
+        
+        vector<int> dp(n+1,0);
+        
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=2;
+        
+        for(int i=3; i<=n; i++)
+        {
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        
+        return dp[n];
     }
 };
