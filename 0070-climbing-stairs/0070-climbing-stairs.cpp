4bfc8322a2de3131377bcaf401c5e150d2f88cb8
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-06-25 19:10:05 +0530

    Time: 3 ms (35.22%), Space: 5.9 MB (96.94%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -32,17 +32,30 @@ public:
             return n;
         }
         
-        vector<int> dp(n+1,0);
+//         vector<int> dp(n+1,0);
         
-        dp[0]=0;
-        dp[1]=1;
-        dp[2]=2;
+//         dp[0]=0;
+//         dp[1]=1;
+//         dp[2]=2;
+        
+//         for(int i=3; i<=n; i++)
+//         {
+//             dp[i]=dp[i-1]+dp[i-2];
+//         }
+        
+//         return dp[n];
+        
+        int x=1;
+        int y=2;
+        int z;
         
         for(int i=3; i<=n; i++)
         {
-            dp[i]=dp[i-1]+dp[i-2];
+            z=x+y;
+            x=y;
+            y=z;
         }
         
-        return dp[n];
+        return z;
     }
 };
