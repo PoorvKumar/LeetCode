c6d75e9eb8ae09a37c001beb8786c554b0db9583
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-07-08 16:37:36 +0530

    Time: 1206 ms (10.26%), Space: 51.3 MB (5.77%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,31 +1,41 @@
 class Solution 
 {
 private:
-    bool canJumpUtil(vector<int>& nums,int target)
-    {
-        if(target==0)
+    vector<int> dp;
+    
+    bool canJumpUtil(vector<int>& nums,int index)
+    { 
+        if(nums[index]+index>=nums.size()-1)
         {
             return true;
         }
         
-        int i=target-1;
-        
-        while(i>=0)
+        if(dp[index]!=-1)
         {
-            if(nums[i]+i>=target)
-            {
-                return canJumpUtil(nums,i);
-            }
-            i--;
+            return dp[index];
         }
         
-        return false;
+        for(int i=1; i<nums[index]+1; i++)
+        {
+            if(canJumpUtil(nums,index+i))
+            {
+                dp[index]=1; 
+                return true; //Top-Down DP approach -> recursion + Memoization
+                //TC: O(n) //as for every index Recursion calls Memoized
+                //SC: O(n)+O(n)auxiliary stack space
+            }
+        }
+        
+        dp[index]=0;
+        return false; //Recursive Solution
+        //TC: O(k^n) //as k Recursion calls for every index
+        //SC: O(n)+O(n)auxiliary stack space
     }
 public:
     bool canJump(vector<int>& nums) 
     {
-        int n=nums.size();
+        dp.assign(nums.size(),-1);
         
-        return canJumpUtil(nums,n-1);
+        return canJumpUtil(nums,0);
     }
 };
