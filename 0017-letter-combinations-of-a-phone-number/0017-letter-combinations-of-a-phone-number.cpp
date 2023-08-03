22ad7c671fbc19a827414fe425d4516262878948
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-08-03 09:37:12 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (18.80%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -3,47 +3,44 @@ class Solution
 private:
     unordered_map<char,vector<string>> umap;
     
-public:
-    Solution()
+    vector<string> letterCombinationUtil(string& digits,int index)
     {
-        umap['2']=vector<string>({"a","b","c"});
-        umap['3']=vector<string>({"d","e","f"});
-        umap['4']=vector<string>({"g","h","i"});
-        umap['5']=vector<string>({"j","k","l"});
-        umap['6']=vector<string>({"m","n","o"});
-        umap['7']=vector<string>({"p","q","r","s"});
-        umap['8']=vector<string>({"t","u","v"});
-        umap['9']=vector<string>({"w","x","y","z"});
-    }
-    
-    vector<string> letterCombinations(string digits) 
-    {
-        if(digits.length()==0)
+        if(index==digits.size()-1)
         {
-            return vector<string>();
-        }
-        if(digits.length()==1)
-        {
-            return umap[digits[0]];
+            return umap[digits[index]];
         }
         
-        vector<string> vec=umap[digits[0]];
-        digits=digits.substr(1,digits.length()-1);
-        vector<string> subres=letterCombinations(digits);
+        vector<string> vec=letterCombinationUtil(digits,index+1);
         
-        vector<string> ans;
+        vector<string> res;
         
-        for(int i=0; i<vec.size(); i++)
+        for(auto s:umap[digits[index]])
         {
-            // string str=vec[i];
-            for(int j=0; j<subres.size(); j++)
+            for(auto str:vec)
             {
-                // str=str+subres[j];
-                // ans.push_back(str);
-                ans.push_back(vec[i]+subres[j]);
+                res.push_back(s+str);
             }
         }
         
-        return ans;
+        return res;
+    }
+public:
+    vector<string> letterCombinations(string digits) 
+    {
+        if(digits.empty())
+        {
+            return vector<string>();
+        }
+        
+        umap['2']={"a","b","c"};
+        umap['3']=vector<string>({"d","e","f"});
+        umap['4']={"g","h","i"};
+        umap['5']=vector<string>({"j","k","l"});
+        umap['6']={"m","n","o"};
+        umap['7']=vector<string>({"p","q","r","s"});
+        umap['8']={"t","u","v"};
+        umap['9']={"w","x","y","z"};
+        
+        return letterCombinationUtil(digits,0);
     }
 };
