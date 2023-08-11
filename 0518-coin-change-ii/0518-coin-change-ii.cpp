2615e41581aed7287ee14b6ffe2c06c9a8435beb
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-08-11 16:03:47 +0530

    Time: 33 ms (49.33%), Space: 18.7 MB (24.11%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,110 +1,51 @@
 class Solution 
 {
 private:
-    unordered_map<int,unordered_map<int,int>> umap; //umap[index][amount]
+    vector<vector<int>> dp;
     
     int changeUtil(vector<int>& coins,int index,int amount)
     {
-        if(amount==0) //base case
+        if(amount<=0)
         {
-            return 1;
+            return amount==0;
         }
         
-        if(index==coins.size()-1) //base case
+        if(index>=coins.size())
         {
-            return amount%coins[index]==0;
+            return amount==0;
         }
         
-        if(umap.count(index) && umap[index].count(amount))
+        if(dp[index][amount]!=-1)
         {
-            return umap[index][amount];
+            return dp[index][amount];
         }
         
+        //exclude
         int exclude=changeUtil(coins,index+1,amount);
-        int include=0;
-        if(amount>=coins[index])
-        {
-            include=changeUtil(coins,index,amount-coins[index]);
-        }
+        
+        //include
+        // int include=0;
+        // if(amount-coins[index]>=0)
+        // {
+        //     include=changeUtil(coins,index,amount-coins[index]);
+        // }
+        int include=changeUtil(coins,index,amount-coins[index]);
         
         int ans=exclude+include;
         // return ans; //Recursive Solution
-        //TC: O(2^n) //as 2 (exclude,include) calls for evry index
-        //SC: O(n)+O(n)auxiliary stack space
+        //TC: O(2^n*amount)
+        //SC: O(n)+O(n*amount)auxiliary stack space
         
-        // umap[index][amount]=ans;
-        return umap[index][amount]=ans; //Top-Down DP approach -> Recursion + Memoization
+        return dp[index][amount]=ans; //Top-Down DP approach -> Recursion + Memoization
         //TC: O(n*amount) //as for every index for every amount Recursion calls Memoized
-        //SC: O(n*amount)+O(n)auxiliary stack space
+        //SC: O(n*amount)+O(n*amount)auxiliary stack space
     }
 public:
     int change(int amount, vector<int>& coins) 
     {
-        sort(coins.begin(),coins.end());
-        
-        // return changeUtil(coins,0,amount);
-        
         int n=coins.size();
-//         vector<vector<int>> dp(n,vector<int>(amount+1,0)); //dp[index][amount]
+        dp.assign(n,vector<int>(amount+1,-1));
         
-//         for(int i=0; i<n; i++) //base case
-//         {
-//             dp[i][0]=1;
-//         }
-        
-//         for(int i=1; i<=amount; i++) //base case
-//         {
-//             dp[n-1][i]=i%coins[n-1]==0;
-//         }
-        
-//         for(int i=n-2; i>=0; i--)
-//         {
-//             for(int j=0; j<=amount; j++)
-//             {
-//                 int exclude=dp[i+1][j];
-//                 int include=0;
-//                 if(j>=coins[i])
-//                 {
-//                     include=dp[i][j-coins[i]];
-//                 }
-                
-//                 dp[i][j]=exclude+include;
-//             }
-//         }
-        
-//         return dp[0][amount]; //Bottom-Up DP approach -> Tabulation
-//         //TC: O(n*amount)
-//         //SC: O(n*amount)
-        
-        vector<int> prev(amount+1,0);
-        vector<int> curr(amount+1,0);
-        
-        prev[0]=1; //base case
-        curr[0]=1; //base case
-        
-        for(int i=1; i<=amount; i++) //base case
-        {
-            prev[i]=i%coins[n-1]==0;
-        }
-        
-        for(int i=n-2; i>=0; i--)
-        {
-            for(int j=1; j<=amount; j++)
-            {
-                int exclude=prev[j];
-                int include=0;
-                if(j>=coins[i])
-                {
-                    include=curr[j-coins[i]];
-                }
-                
-                curr[j]=exclude+include;
-            }
-            prev=curr;
-        }
-        
-        return prev[amount]; //Space Optimisation
-        //TC: O(n*amount)
-        //SC: O(n)
+        return changeUtil(coins,0,amount);
     }
 };
