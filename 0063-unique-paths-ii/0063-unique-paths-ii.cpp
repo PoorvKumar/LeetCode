5713ef38da6a66d1c6a245172dfdbcd41a8bf46d
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-08-12 09:48:56 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (81.25%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -55,9 +55,35 @@ public:
             return 0;
         }
         
-        vector<vector<long long>> dp;
-        dp.assign(m+1,vector<long long>(n+1,0));
-        dp[m-1][n-1]=1;
+//         vector<vector<long long>> dp;
+//         dp.assign(m+1,vector<long long>(n+1,0));
+//         dp[m-1][n-1]=1;
+        
+//         for(int i=m-1; i>=0; i--)
+//         {
+//             for(int j=n-1; j>=0; j--)
+//             {
+//                 if(obstacleGrid[i][j]==1)
+//                 {
+//                     continue;
+//                 }
+                
+//                 long long right=dp[i][j+1];
+//                 long long down=dp[i+1][j];
+                
+//                 long long ans=right+down;
+//                 dp[i][j]+=ans;
+//             }
+//         }
+        
+//         return dp[0][0]; //Bottom-Up DP approach -> Tabulation
+//         //TC: O(m*n)
+//         //SC: O(m*n)+O(m*n)
+        
+        vector<long long> curr(n+1,0);
+        vector<long long> prev(n+1,0);
+        
+        curr[n-1]=1;
         
         for(int i=m-1; i>=0; i--)
         {
@@ -68,14 +94,18 @@ public:
                     continue;
                 }
                 
-                long long right=dp[i][j+1];
-                long long down=dp[i+1][j];
+                long long right=curr[j+1];
+                long long down=prev[j];
                 
                 long long ans=right+down;
-                dp[i][j]+=ans;
+                curr[j]+=ans;
             }
+            prev=curr;
+            curr.assign(n+1,0);
         }
         
-        return dp[0][0];
+        return prev[0]; //Space Optimization
+        //TC: O(m*n)
+        //SC: O(m*n)+O(n)
     }
 };
