b626644dcf7059a631293fe3b8c2c063f19a41c6
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-08-21 23:37:51 +0530

    Time: 181 ms (5.40%), Space: 108.6 MB (5.30%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -45,8 +45,35 @@ public:
     {
         int n=nums.size();
         
-        dp.assign(n+1,vector<int>(2,-1));
+        // dp.assign(n+1,vector<int>(2,-1));
         
-        return maxSubArrayUtil(nums,0,false);
+        // return maxSubArrayUtil(nums,0,false);
+        
+        dp.assign(n+1,vector<int>(2,0));
+        
+        dp[n][0]=INT_MIN; //base case
+        dp[n][1]=0; //base case
+        
+        for(int i=n-1; i>=0; i--)
+        {
+            for(int j=1; j>=0; j--)
+            {
+                if(j)
+                {
+                    int exclude=0;
+                    int include=nums[i]+dp[i+1][j];
+                    
+                    dp[i][j]=max(exclude,include);
+                    continue;
+                }
+                
+                int exclude=dp[i+1][j];
+                int include=nums[i]+dp[i+1][!j];
+                
+                dp[i][j]=max(exclude,include);
+            }
+        }
+        
+        return dp[0][0];
     }
 };
