382d1588bf227590dcf9197394ac53214b34459c
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-08-21 23:29:59 +0530

    Time: 989 ms (5.40%), Space: 353.3 MB (5.30%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,22 +1,40 @@
 class Solution 
 {
+private:
+    unordered_map<int,unordered_map<bool,int>> umap; //umap[index][pick]
+    
+    int maxSubArrayUtil(vector<int>& nums,int index,bool pick)
+    {
+        if(index>=nums.size())
+        {
+            return pick?0:INT_MIN;
+        }
+        
+        if(umap.count(index) && umap[index].count(pick))
+        {
+            return umap[index][pick];
+        }
+        
+        if(pick)
+        {
+            int exclude=0; //once excluded after picking we stop
+            int include=nums[index]+maxSubArrayUtil(nums,index+1,pick);
+            
+            // return max(exclude,include);
+            
+            return umap[index][pick]=max(exclude,include);
+        }
+        
+        int exclude=maxSubArrayUtil(nums,index+1,pick);
+        int include=nums[index]+maxSubArrayUtil(nums,index+1,!pick);
+        
+        // return max(exclude,include);
+        
+        return umap[index][pick]=max(exclude,include);
+    }
 public:
     int maxSubArray(vector<int>& nums) 
     {
-        int sum=0;
-        int maxim=INT_MIN;
-        
-        for(auto x:nums)
-        {
-            sum=sum+x;
-            maxim=max(maxim,sum);
-            
-            if(sum<0)
-            {
-                sum=0;
-            }
-        }
-        
-        return maxim;
+        return maxSubArrayUtil(nums,0,false);
     }
 };
