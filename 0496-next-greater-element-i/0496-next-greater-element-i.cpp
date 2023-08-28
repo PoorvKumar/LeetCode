1676f58980f8c6720dd383019f1432a47847016f
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-08-29 00:26:48 +0530

    Time: 3 ms (90.76%), Space: 8.8 MB (75.21%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -6,13 +6,16 @@ public:
         stack<int> stk;
         unordered_map<int,int> umap;
         
-        for(int i=0; i<nums2.size(); i++)
+        stk.push(nums2[0]);
+        
+        for(int i=1; i<nums2.size(); i++)
         {
             while(!stk.empty() && stk.top()<nums2[i])
             {
                 umap[stk.top()]=nums2[i];
                 stk.pop();
             }
+            
             stk.push(nums2[i]);
         }
         
@@ -21,13 +24,13 @@ public:
             if(umap.count(x))
             {
                 x=umap[x];
-                continue;
             }
-            x=-1;
+            else
+            {
+                x=-1;
+            }
         }
         
-        return nums1; //monotonic decreasing stack
-        //TC: O(m+n)
-        //SC: O(n)
+        return nums1;
     }
 };
