5671978421324726579daefd9d63b3a6e8bd2b37
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-09-01 15:58:23 +0530

    Time: 3 ms (89.82%), Space: 9 MB (5.98%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,14 +1,45 @@
 class Solution 
 {
+private:
+    vector<int> dp;
+    
+    int countBitsUtil(int n)
+    {
+        if(n==0)
+        {
+            return 0;
+        }
+        
+        if(dp[n]!=-1)
+        {
+            return dp[n];
+        }
+        
+        // return n%2+countBitsUtil(n/2);
+        
+        return dp[n]=n%2+countBitsUtil(n/2);
+    }
 public:
     vector<int> countBits(int n) 
     {
+//         vector<int> res;
+        
+//         for(int i=0; i<=n; i++)
+//         {
+//             bitset<32> bs(i);
+//             res.push_back(bs.count());
+//         }
+        
+//         return res;
+        
+        dp.assign(n+1,-1);
+        
         vector<int> res;
         
         for(int i=0; i<=n; i++)
         {
-            bitset<32> bs(i);
-            res.push_back(bs.count());
+            int count=countBitsUtil(i);
+            res.push_back(count);
         }
         
         return res;
