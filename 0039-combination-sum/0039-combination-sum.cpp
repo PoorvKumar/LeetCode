a1ba4f5e396e6e3b67de405402133eb2818f86e1
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-09-01 16:34:55 +0530

    Time: 7 ms (58.60%), Space: 17.1 MB (29.52%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,25 +1,32 @@
 class Solution 
 {
 private:
-    void combinationSumUtil(vector<int>& c,vector<vector<int>>& res,vector<int>& vec,int target,int index)
+    vector<vector<int>> res;
+    vector<int> vec;
+    
+    void combinationSumUtil(vector<int>& candidates,int index,int target)
     {
-        if(target<0 || index>=c.size())
+        if(target<=0)
+        {
+            if(target==0 && !vec.empty())
+            {
+                res.push_back(vec);
+            }
+            
+            return ;
+        }
+        
+        if(index>=candidates.size())
         {
             return ;
         }
         
-        if(target==0)
-        {
-            res.push_back(vec);
-            return ;
-        }
-        
-        //not include
-        combinationSumUtil(c,res,vec,target,index+1);
+        //exclude
+        combinationSumUtil(candidates,index+1,target);
         
         //include
-        vec.push_back(c[index]);
-        combinationSumUtil(c,res,vec,target-c[index],index);
+        vec.push_back(candidates[index]);
+        combinationSumUtil(candidates,index,target-candidates[index]);
         vec.pop_back();
         
         return ;
@@ -27,10 +34,7 @@ private:
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) 
     {
-        vector<vector<int>> res;
-        vector<int> vec;
-        
-        combinationSumUtil(candidates,res,vec,target,0);
+        combinationSumUtil(candidates,0,target);
         
         return res;
     }
