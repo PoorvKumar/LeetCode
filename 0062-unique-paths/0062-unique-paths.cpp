5768530c342176a3bca5d5704fec7549484a557b
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-09-03 10:52:19 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (54.47%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -34,8 +34,29 @@ private:
 public:
     int uniquePaths(int m, int n) 
     {
-        dp.assign(m,vector<int>(n,-1));
+//         dp.assign(m,vector<int>(n,-1));
         
-        return uniquePathsUtil(m,n,0,0);
+//         return uniquePathsUtil(m,n,0,0);
+        
+        dp.assign(m+1,vector<int>(n+1,0));
+        dp[m-1][n-1]=1;
+        
+        for(int i=m-1; i>=0; i--)
+        {
+            for(int j=n-1; j>=0; j--)
+            {
+                if(i==m-1 && j==n-1)
+                {
+                    continue;
+                }
+                
+                int down=dp[i+1][j];
+                int right=dp[i][j+1];
+                
+                dp[i][j]=down+right;
+            }
+        }
+        
+        return dp[0][0];
     }
 };
