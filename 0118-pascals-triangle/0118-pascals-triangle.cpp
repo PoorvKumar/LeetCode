0297f6b496f0108973815e10da66b2041afc4dd4
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-09-08 10:08:00 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (82.50%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,22 +1,38 @@
-class Solution {
+class Solution 
+{
+private:
+    vector<int> generateUtil(int n,vector<int>& last)
+    {
+        vector<int> vec(n,0);
+        vec[0]=1;
+        vec.back()=1;
+        
+        for(int i=1; i<n-1; i++)
+        {
+            vec[i]=last[i-1]+last[i];
+        }
+        
+        return vec;
+    }
 public:
     vector<vector<int>> generate(int numRows) 
     {
-        vector<vector<int>> ans(numRows);
+        vector<vector<int>> res;
+        res.push_back({1});
         
-        ans[0]=vector<int>(1,1);
-        // ans[0]={1};
-        
-        for(int i=1; i<numRows; i++)
+        if(numRows==1)
         {
-            ans[i]=vector<int>(i+1,1); //{1 , , , , 1}
-            
-            for(int j=1; j<i; j++)
-            {
-                ans[i][j]=ans[i-1][j-1]+ans[i-1][j];
-            }
+            return res;
         }
         
-        return ans;
+        for(int i=2; i<=numRows; i++)
+        {
+            vector<int> vec=generateUtil(i,res.back());
+            res.push_back(vec);
+        }
+        
+        return res; 
+        //TC: O(n^2)
+        //SC: O(n^2)
     }
 };
