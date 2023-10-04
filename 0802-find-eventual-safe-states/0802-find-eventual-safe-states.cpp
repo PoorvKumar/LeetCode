2f228a8e9621c95561dbeac5c434c3d7aa7e7c2a
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-10-04 22:55:44 +0530

    Time: 129 ms (64.62%), Space: 47.1 MB (93.03%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,46 +1,62 @@
-class Solution {
-public:
-    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int n = graph.size();
-        vector<int> indegree(n);
-        vector<vector<int>> adj(n);
-
-        for (int i = 0; i < n; i++) {
-            for (auto node : graph[i]) {
-                adj[node].push_back(i);
-                indegree[i]++;
+class Solution 
+{
+private:
+    int n;
+    vector<bool> visited;
+    vector<bool> isSafe;
+    
+    bool checkSafe(vector<vector<int>>& graph,int src)
+    {
+        visited[src]=true;
+        
+        for(auto x:graph[src])
+        {
+            if(visited[x] && !isSafe[x])
+            {
+                return false;
             }
-        }
-
-        queue<int> q;
-        // Push all the nodes with indegree zero in the queue.
-        for (int i = 0; i < n; i++) {
-            if (indegree[i] == 0) {
-                q.push(i);
-            }
-        }
-
-        vector<bool> safe(n);
-        while (!q.empty()) {
-            int node = q.front();
-            q.pop();
-            safe[node] = true;
-
-            for (auto& neighbor : adj[node]) {
-                // Delete the edge "node -> neighbor".
-                indegree[neighbor]--;
-                if (indegree[neighbor] == 0) {
-                    q.push(neighbor);
+            
+            if(!isSafe[x])
+            {
+                if(!checkSafe(graph,x))
+                {
+                    return false;
                 }
             }
         }
-
-        vector<int> safeNodes;
-        for(int i = 0; i < n; i++) {
-            if(safe[i]) {
-                safeNodes.push_back(i);
+        
+        return isSafe[src]=true;
+    }
+public:
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) 
+    {
+        n=graph.size();
+        visited.assign(n,false);
+        isSafe.assign(n,false);
+        
+        for(int i=0; i<n; i++)
+        {
+            if(!visited[i])
+            {
+                if(!isSafe[i])
+                {
+                    if(checkSafe(graph,i))
+                    {
+                        isSafe[i]=true;
+                    }
+                }
             }
         }
-        return safeNodes;
+        
+        vector<int> res;
+        for(int i=0; i<n; i++)
+        {
+            if(isSafe[i])
+            {
+                res.push_back(i);
+            }
+        }
+        
+        return res;
     }
 };
