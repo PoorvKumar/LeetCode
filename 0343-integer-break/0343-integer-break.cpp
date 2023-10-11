92bdb1c1b9fa9a1d8c1bfc6e81a9d40d4a9ad6e5
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-10-11 15:09:06 +0530

    Time: 3 ms (19.63%), Space: 6.6 MB (37.09%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,31 +1,48 @@
-class Solution {
-public:
-    vector<int> memo;
+class Solution 
+{
+private:
+    vector<int> dp;
     
-    int integerBreak(int n) {
-        if (n <= 3) {
-            return n - 1;
+    int integerBreakUtil(int n)
+    {
+        if(n==1)
+        {
+            return n;
         }
         
-        memo = vector(n + 1, 0);
-        return dp(n);
+        if(dp[n]!=-1)
+        {
+            return dp[n];
+        }
+        
+        int ans=n;
+        for(int i=1; i<n; i++)
+        {
+            int val=i*integerBreakUtil(n-i);
+            ans=max(ans,val);
+        }
+        
+        // return ans; //Recursive Solution
+        //TC: O(n*n) //as for every n , n Recursion calls
+        //SC: O(n)
+        
+        return dp[n]=ans; //Top-Down DP approach -> Recursion + Memoization
+        //TC: O(n) //as for every n Recursion calls Memoized
+        //SC: O(n)
     }
-    
-    int dp(int num) {
-        if (num <= 3) {
-            return num;
+public:
+    int integerBreak(int n) 
+    {
+        dp.assign(n+1,-1);
+        
+        int ans=1;
+        
+        for(int i=1; i<n; i++)
+        {
+            int val=i*integerBreakUtil(n-i);
+            ans=max(ans,val);
         }
         
-        if (memo[num] != 0) {
-            return memo[num];
-        }
-        
-        int ans = num;
-        for (int i = 2; i < num; i++) {
-            ans = max(ans, i * dp(num - i));
-        }
-        
-        memo[num] = ans;
         return ans;
     }
 };
