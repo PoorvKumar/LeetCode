e72cb18615fcac7888d351147ca6eb79522dbb33
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-10-15 17:51:15 +0530

    Time: 17 ms (64.93%), Space: 16.2 MB (28.35%) - LeetHub

--- 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
+++ 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
@@ -46,7 +46,7 @@ private:
 public:
     int numWays(int steps, int arrLen) 
     {
-        dp.assign(steps+1,vector<int>(505,-1)); //arrLen fixed to 505
+        dp.assign(steps+1,vector<int>(501,-1)); //arrLen fixed to 505 //saw in discuss section
         
         return numWaysUtil(0,steps,arrLen);
         
