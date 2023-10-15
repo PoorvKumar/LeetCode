d8a39a9435b1c2fdce3830c3580fb7402715544f
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-10-15 17:57:01 +0530

    Time: 11 ms (82.68%), Space: 6.5 MB (93.94%) - LeetHub

--- 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
+++ 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
@@ -50,76 +50,76 @@ public:
         
 //         return numWaysUtil(0,steps,arrLen);
         
-        dp.assign(501,vector<int>(501,0));
-        dp[0][0]=1;
-        
-        for(int i=1; i<=steps; i++)
-        {
-            for(int k=min(arrLen,500); k>=0; k--)
-            {
-                if(k-i>0)
-                {
-                    continue;
-                }
-                
-                int stay=dp[i-1][k];
-                
-                int left=0;
-                if(k-1>=0)
-                {
-                    left=dp[i-1][k-1];
-                }
-                
-                int right=0;
-                if(k+1<min(arrLen,500))
-                {
-                    right=dp[i-1][k+1];
-                }
-                
-                int ans=(stay%M+left%M+right%M)%M;
-                dp[i][k]=ans;
-            }
-        }
-        
-        return dp[steps][0]; //Bottom-Up DP approach -> Tabulation
-        // TC: O(steps*arrLen)
-        // SC: O(steps*arrLen)
-        
-//         vector<int> prev(501,0);
-//         vector<int> curr(501,0);
-//         prev[0]=1;
+//         dp.assign(501,vector<int>(501,0));
+//         dp[0][0]=1;
         
 //         for(int i=1; i<=steps; i++)
 //         {
-//             for(int k=arrLen-1; k>=0; k--)
+//             for(int k=min(arrLen-1,500); k>=0; k--)
 //             {
 //                 if(k-i>0)
 //                 {
 //                     continue;
 //                 }
                 
-//                 int stay=prev[k];
+//                 int stay=dp[i-1][k];
                 
 //                 int left=0;
 //                 if(k-1>=0)
 //                 {
-//                     left=prev[k-1];
+//                     left=dp[i-1][k-1];
 //                 }
                 
 //                 int right=0;
-//                 if(k+1<arrLen)
+//                 if(k+1<min(arrLen,500))
 //                 {
-//                     right=prev[k+1];
+//                     right=dp[i-1][k+1];
 //                 }
                 
 //                 int ans=(stay%M+left%M+right%M)%M;
-//                 curr[k]=ans;
+//                 dp[i][k]=ans;
 //             }
-//             prev=curr;
 //         }
         
-//         return prev[0]; //Space Optimisation
-//         //TC: O(steps*arrLen)
-//         //SC: O(arrLen)
+//         return dp[steps][0]; //Bottom-Up DP approach -> Tabulation
+//         // TC: O(steps*arrLen)
+//         // SC: O(steps*arrLen)
+        
+        vector<int> prev(501,0);
+        vector<int> curr(501,0);
+        prev[0]=1;
+        
+        for(int i=1; i<=steps; i++)
+        {
+            for(int k=min(arrLen-1,500); k>=0; k--)
+            {
+                if(k-i>0)
+                {
+                    continue;
+                }
+                
+                int stay=prev[k];
+                
+                int left=0;
+                if(k-1>=0)
+                {
+                    left=prev[k-1];
+                }
+                
+                int right=0;
+                if(k+1<min(arrLen,501))
+                {
+                    right=prev[k+1];
+                }
+                
+                int ans=(stay%M+left%M+right%M)%M;
+                curr[k]=ans;
+            }
+            prev=curr;
+        }
+        
+        return prev[0]; //Space Optimisation
+        //TC: O(steps*arrLen)
+        //SC: O(arrLen)
     }
 };
