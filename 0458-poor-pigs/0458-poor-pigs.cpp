6b4fb5899759d0a469c8625aa82102bfc954dd69
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-10-30 00:30:01 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (25.93%) - LeetHub

---
+++ 0458-poor-pigs/0458-poor-pigs.cpp
@@ -0,0 +1,40 @@
+class Solution 
+{
+public:
+    int poorPigs(int buckets, int minutesToDie, int minutesToTest) 
+    {
+        if(buckets==1)
+        {
+            return 0;
+        }
+        
+        int rounds=minutesToTest/minutesToDie;
+        
+        int low=1;
+        int high=buckets;
+        int mid=(low+high)/2;
+        
+        int val=high;
+        while(pow(rounds+1,val)>=buckets)
+        {
+            val--;
+        }
+        
+        return val+1;
+        
+//         while(low<high)
+//         {
+//             if(pow(rounds+1,mid)>=buckets)
+//             {
+//                 high=mid-1;
+//             }
+//             else
+//             {
+//                 low=mid;
+//             }
+//             mid=(low+high)/2;
+//         }
+        
+//         return mid;
+    }
+};
