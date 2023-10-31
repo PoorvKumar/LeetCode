3d646250fdd8512111e02ce85882b59a8b1ac2d3
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-10-31 13:12:04 +0530

    Time: 246 ms (5.68%), Space: 79.6 MB (34.76%) - LeetHub

--- 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.cpp
+++ 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.cpp
@@ -8,12 +8,12 @@ public:
         
         for(int i=1; i<pref.size(); i++)
         {
-            bitset<32> bs1(pref[i-1]);
-            bitset<32> bs2(pref[i]);
+            bitset<21> bs1(pref[i-1]);
+            bitset<21> bs2(pref[i]);
             
-            bitset<32> bs(0);
+            bitset<21> bs(0);
             
-            for(int i=0; i<32; i++)
+            for(int i=0; i<21; i++)
             {
                 if(bs2[i]==1)
                 {
