9bccbef2470a7810ba70ebed82e84f0df03757ed
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-11-03 10:00:12 +0530

    Time: 0 ms (100.00%), Space: 9 MB (11.73%) - LeetHub

---
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.cpp
@@ -0,0 +1,41 @@
+class Solution 
+{
+private:
+    vector<string> res;
+    
+    void buildArrayUtil(vector<int>& target,int index,int val,int n)
+    {
+        if(index==target.size())
+        {
+            return ;
+        }
+        
+        if(val>n)
+        {
+            return ;
+        }
+        
+        if(target[index]==val)
+        {
+            res.push_back("Push");
+            buildArrayUtil(target,index+1,val+1,n);
+        }
+        else
+        {
+            res.push_back("Push");
+            res.push_back("Pop");
+            buildArrayUtil(target,index,val+1,n);
+        }
+        
+        return ; //Recursive Solution
+        //TC: O(n)
+        //SC: O(n)
+    }
+public:
+    vector<string> buildArray(vector<int>& target, int n) 
+    {
+        buildArrayUtil(target,0,1,n);
+        
+        return res;
+    }
+};
