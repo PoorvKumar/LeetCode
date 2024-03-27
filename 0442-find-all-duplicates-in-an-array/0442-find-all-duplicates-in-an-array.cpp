1b28c6d2b650ec9b5f9c125033035fd9b21fc308
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2024-03-27 14:40:24 +0530

    Time: 45 ms (47.44%), Space: 36 MB (66.88%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
@@ -5,18 +5,34 @@ public:
     {
         vector<int> res;
         
-        //constant extra soace
-        //linear time
-        vector<bool> hasAppeared(1e5+1,false); //technically constant space as we know the upper limit of int n;
+//         //constant extra soace
+//         //linear time
+//         vector<bool> hasAppeared(1e5+1,false); //technically constant space as we know the upper limit of int n;
         
-        for(auto x:nums)
+//         for(auto x:nums)
+//         {
+//             if(hasAppeared[x])
+//             {
+//                 res.push_back(x);
+//             }
+            
+//             hasAppeared[x]=true;
+//         }
+        
+//         return res;
+        
+        for(int i=0; i<nums.size(); i++)
         {
-            if(hasAppeared[x])
+            int x=abs(nums[i]);
+            
+            if(nums[x-1]<0)
             {
                 res.push_back(x);
             }
-            
-            hasAppeared[x]=true;
+            else
+            {
+                nums[x-1]*=-1;
+            }
         }
         
         return res;
